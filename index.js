811e09feda35213b0af42a6d35dacab9de81536d
Safiyya <safiyya@Noahs-MacBook-Pro.local>
2016-10-18 17:20:20 -0700

    Node app working

---
+++ index.js
@@ -0,0 +1,19 @@
+var path = require('path');
+var express = require('express');
+var logger = require('morgan');
+var app = express();
+
+// Log the requests
+app.use(logger('dev'));
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public'))); 
+
+// Route for everything else.
+app.get('*', function(req, res){
+  res.sendFile(path.join(__dirname+'/views/index.html'));
+});
+
+// Fire it up!
+app.listen(3000);
+console.log('Listening on port 3000');
