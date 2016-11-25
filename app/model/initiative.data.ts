03388f914a1b1a5b05615cbd9f0272c84c2a6d69
Safiyya Babio <safiyya.babio@gmail.com>
2016-11-25 15:41:47 -0800

    Adds loading div

--- app/model/initiative.data.ts
+++ app/model/initiative.data.ts
@@ -1,46 +1,45 @@
-import {Injectable, Inject} from '@angular/core'
+import { Injectable, Inject } from '@angular/core'
 
 @Injectable()
-export class InitiativeNode{
-    
-    id:number;
-    name:string;
-    isRoot:boolean=false;
-    hasFocus:boolean=false;
-    isZoomedOn:boolean= false;
-    isSearchedFor:boolean=false;
-    description:string= undefined;
-    private _size:number = undefined ; //(this.children === undefined ? 0 : this.children.length);
-    children:Array<InitiativeNode>;
+export class InitiativeNode {
 
-    constructor(){}
+    id: number;
+    name: string;
+    isRoot: boolean = false;
+    hasFocus: boolean = false;
+    isZoomedOn: boolean = false;
+    isSearchedFor: boolean = false;
+    description: string = undefined;
+    private _size: number = undefined; //(this.children === undefined ? 0 : this.children.length);
+    children: Array<InitiativeNode>;
 
-    get size():number {
+    constructor() { }
+
+    get size(): number {
         return this._size || 1;
     }
 
-    set size(size:number){
+    set size(size: number) {
         this._size = size;
     }
 
-   static traverse(node : InitiativeNode,callback:((n:InitiativeNode) => void) ) {
-       if(node.children)
-       {
-           node.children.forEach(function(child){
-                callback.apply(this,[child]);  
+    static traverse(node: InitiativeNode, callback: ((n: InitiativeNode) => void)) {
+        if (node.children) {
+            node.children.forEach(function (child) {
+                callback.apply(this, [child]);
                 InitiativeNode.traverse(child, callback);
             })
-       }
+        }
     }
 
-    static resetZoomedOn(nodes:Array<InitiativeNode>){
-         nodes.forEach(function (n: InitiativeNode) {
+    static resetZoomedOn(nodes: Array<InitiativeNode>) {
+        nodes.forEach(function (n: InitiativeNode) {
             n.isZoomedOn = false;
             InitiativeNode.traverse(n, function (node) { node.isZoomedOn = false });
         });
     }
 
-     static resetSearchedFor(nodes:Array<InitiativeNode>){
+    static resetSearchedFor(nodes: Array<InitiativeNode>) {
         nodes.forEach(function (n: InitiativeNode) {
             n.isSearchedFor = false;
             InitiativeNode.traverse(n, function (node) { node.isSearchedFor = false });
