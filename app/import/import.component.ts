81e25489324be90581a97a9b821743394ae2bbf9
Safiyya Babio <safiyya.babio@gmail.com>
2016-12-25 15:03:36 -0600

    Removes import

--- app/import/import.component.ts
+++ app/import/import.component.ts
@@ -1,37 +1,37 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { ModalComponent } from 'ng2-bs3-modal/ng2-bs3-modal';
-import { FileUploader, FileSelectDirective , FileDropDirective} from 'ng2-file-upload';
+// import { Component, OnInit, ViewChild } from '@angular/core';
+// import { ModalComponent } from 'ng2-bs3-modal/ng2-bs3-modal';
+// import { FileUploader, FileSelectDirective , FileDropDirective} from 'ng2-file-upload';
 
-// const URL = '/api/';
-const URL = 'https://evening-anchorage-3159.herokuapp.com/api/';
+// // const URL = '/api/';
+// const URL = 'https://evening-anchorage-3159.herokuapp.com/api/';
 
 
-@Component({
-    selector: 'import',
-    templateUrl: 'import.component.html'
-})
-export class ImportComponent implements OnInit {
+// @Component({
+//     selector: 'import',
+//     templateUrl: 'import.component.html'
+// })
+// export class ImportComponent implements OnInit {
 
-    @ViewChild('importModal')
-    modal: ModalComponent;
+//     @ViewChild('importModal')
+//     modal: ModalComponent;
 
-    constructor() { }
+//     constructor() { }
 
-    ngOnInit() { }
+//     ngOnInit() { }
 
-    open() {
-        this.modal.open();
-    }
+//     open() {
+//         this.modal.open();
+//     }
 
-    public uploader: FileUploader = new FileUploader({ url: URL });
-    public hasBaseDropZoneOver: boolean = false;
-    public hasAnotherDropZoneOver: boolean = false;
+//     public uploader: FileUploader = new FileUploader({ url: URL });
+//     public hasBaseDropZoneOver: boolean = false;
+//     public hasAnotherDropZoneOver: boolean = false;
 
-    public fileOverBase(e: any): void {
-        this.hasBaseDropZoneOver = e;
-    }
+//     public fileOverBase(e: any): void {
+//         this.hasBaseDropZoneOver = e;
+//     }
 
-    public fileOverAnother(e: any): void {
-        this.hasAnotherDropZoneOver = e;
-    }
-}
+//     public fileOverAnother(e: any): void {
+//         this.hasAnotherDropZoneOver = e;
+//     }
+// }
