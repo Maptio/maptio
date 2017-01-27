f4ae8bd803fe18a214f831f71601790b335c579b
Safiyya Babio <safiyya.babio@gmail.com>
2017-01-27 16:45:53 -0500

    Adds test for initiative.data

--- test/specs/model/initiative.data.spec.ts
+++ test/specs/model/initiative.data.spec.ts
@@ -1,9 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { InitiativeNode } from '../../../app/model/initiative.data';
 
-describe('Initiative Tests', () => {
+describe('initiative.data.ts', () => {
  
+    
+
     beforeEach(() => {
     });
     
+    it('Leaves description undefined at creation', () => {
+        let target = new InitiativeNode();
+        expect(target.description).toBeUndefined();
+    });
+
 });
