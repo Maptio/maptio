e37a5dc38cb2a8f4fb9a75c155798fb45539912c
Safiyya Babio <safiyya.babio@gmail.com>
2017-01-27 16:45:12 -0500

    Deleted extra test files

--- app/model/initiative.data.spec.ts
+++
@@ -1,9 +0,0 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { InitiativeNode } from './initiative.data';
-
-describe('Initiative Tests', () => {
- 
-    beforeEach(() => {
-    });
-    
-});
