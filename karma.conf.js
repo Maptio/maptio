f58bb03760ff3cde6c7c85b61b7c34d6c07c595b
Safiyya Babio <safiyya.babio@gmail.com>
2017-02-24 15:57:38 -0800

    restore console.log output when testing

--- karma.conf.js
+++ karma.conf.js
@@ -9,7 +9,7 @@ module.exports = function (config) {
             { pattern: "./base.spec.ts" },
 
             { pattern: "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js" },
-            {pattern: "https://cdn.auth0.com/js/lock/10.8/lock.min.js"},
+            { pattern: "https://cdn.auth0.com/js/lock/10.8/lock.min.js" },
 
             { pattern: "./app/app.component.ts" },
             { pattern: "./app/shared/interfaces/*.ts" },
@@ -46,20 +46,25 @@ module.exports = function (config) {
             '**/*.json': ['json_fixtures']
         },
 
+        browserConsoleLogOptions: {
+            terminal: true,
+            level: ""
+        }, 
+        
         karmaTypescriptConfig: {
             bundlerOptions: {
                 // ignore: ["d3-ng2-service"],
-                 validateSyntax: false
+                validateSyntax: false
             },
-            reports: { 
+            reports: {
                 "html": {
                     "directory": "test/coverage",
-                    "subdirectory":"html",
+                    "subdirectory": "html",
                     "filename": "coverage.html"
                 },
-                "lcovonly":{
+                "lcovonly": {
                     "directory": "test/coverage",
-                    "subdirectory":"lcov",
+                    "subdirectory": "lcov",
                     "filename": "lcov.info"
                 }
             }
