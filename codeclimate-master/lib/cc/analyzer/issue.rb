37c68043b5feace02932e8368efb3f22faec0e1b
Safiyya Babio <safiyya.babio@gmail.com>
2017-03-14 22:44:41 -0700

    Cleaning code

---
+++ codeclimate-master/lib/cc/analyzer/issue.rb
@@ -0,0 +1,54 @@
+module CC
+  module Analyzer
+    class Issue
+      SPEC_ISSUE_ATTRIBUTES = %w[
+        categories
+        check_name
+        content
+        description
+        location
+        other_locations
+        remediation_points
+        severity
+        type
+      ]
+
+      def initialize(output)
+        @output = output
+      end
+
+      def as_json(*)
+        parsed_output.reverse_merge!(
+          "fingerprint" => fingerprint,
+        )
+      end
+
+      def fingerprint
+        parsed_output.fetch("fingerprint") { default_fingerprint }
+      end
+
+      # Allow access to hash keys as methods
+      SPEC_ISSUE_ATTRIBUTES.each do |key|
+        define_method(key) do
+          parsed_output[key]
+        end
+      end
+
+      def path
+        parsed_output.fetch("location", {}).fetch("path", "")
+      end
+
+      private
+
+      attr_reader :output
+
+      def default_fingerprint
+        SourceFingerprint.new(self).compute
+      end
+
+      def parsed_output
+        @parsed_output ||= JSON.parse(output)
+      end
+    end
+  end
+end
