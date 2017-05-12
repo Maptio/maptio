4b42f7dbaa5b2ec7b7ec1f31611fb28500f0c5e9
Safiyya <safiyya.babio@gmail.com>
2017-05-12 09:53:29 -0700

    Redirect http to https --  revert

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require('newrelic');
 
-const sslRedirect = require('heroku-ssl-redirect');
+// const sslRedirect = require('heroku-ssl-redirect');
 const bodyParser = require('body-parser');
 const path = require('path');
 const express = require('express');
@@ -19,7 +19,7 @@ const app = express(),
 
 
 app.use(bodyParser.json());
-app.use(sslRedirect);
+// app.use(sslRedirect);
 
 var datasets = require('./routes/datasets');
 var users = require('./routes/users');
@@ -55,9 +55,9 @@ if (isDevelopment) {
   app.use(express.static(DIST_DIR));
   app.get("*", function (req, res, next) {
     // console.log("PRODUCTION")
-    // if (req.protocol !== 'https') {
-    //   return res.redirect(['https://', req.get('Host'), req.url].join(''));
-    // }
+    if (req.protocol !== 'https') {
+      return res.redirect(['https://', req.get('Host'), req.url].join(''));
+    }
     res.sendFile(HTML_FILE)
   }
   )
