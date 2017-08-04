579f6fd15361683be53a12eff655c960925c7cdb
Safiyya <safiyya.babio@gmail.com>
2017-08-04 14:15:33 -0700

    adds first person view

---
+++ src/app/components/mapping/first-person/mapping.first-person.component.ts
@@ -0,0 +1,60 @@
+import { User } from './../../../shared/model/user.data';
+import { Auth } from "./../../../shared/services/auth/auth.service";
+import { Initiative } from "./../../../shared/model/initiative.data";
+import { D3Service, D3 } from "d3-ng2-service";
+import { Observable } from "rxjs/Rx";
+import { IDataVisualizer } from "./../mapping.interface";
+import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef } from "@angular/core";
+import { ColorService } from "../../../shared/services/ui/color.service";
+import { UIService } from "../../../shared/services/ui/ui.service";
+
+@Component({
+    selector: "first-person",
+    template: require("./mapping.first-person.component.html"),
+    styleUrls: ["./mapping.first-person.component.css"],
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+
+export class MappingFirstPersonComponent implements OnInit, IDataVisualizer {
+    width: number;
+    height: number;
+    margin: number;
+    zoom$: Observable<number>;
+    fontSize$: Observable<number>;
+    user: Promise<User>;
+
+    authorities: Array<Initiative> = [];
+    helps: Array<Initiative> = [];
+
+    constructor(public auth: Auth, public cd: ChangeDetectorRef) {
+        // this.auth.getUser().subscribe((user: User) => {
+        //     this.user = user;
+        //     cd.markForCheck();
+        // })
+    }
+
+    ngOnInit() {
+        console.log("ngOnInit")
+    }
+
+    draw(data: Initiative): void {
+        this.auth.getUser().subscribe((user: User) => {
+            this.user = Promise.resolve(user);
+
+            this.user.then((user: User) => {
+                console.log(user)
+                data.traverse(function (i: Initiative) {
+                    if (i.accountable && i.accountable.user_id === user.user_id) {
+                        if (!this.authorities.includes(i)) this.authorities.push(i)
+                    }
+                    if (i.helpers && i.helpers.find(h => h.user_id === user.user_id)) {
+                        if (!this.helps.includes(i)) this.helps.push(i)
+                    }
+                }.bind(this))
+            });
+
+            this.cd.markForCheck();
+        });
+
+    }
+}
--- src/app/components/mapping/mapping.component.ts
+++ src/app/components/mapping/mapping.component.ts
@@ -1,3 +1,4 @@
+import { MappingFirstPersonComponent } from "./first-person/mapping.first-person.component";
 import { ActivatedRoute, Params } from "@angular/router";
 import {
     Component,
@@ -22,7 +23,7 @@ import { Subject, BehaviorSubject, Subscription } from "rxjs/Rx";
     selector: "mapping",
     template: require("./mapping.component.html"),
     styleUrls: ["./mapping.component.css"],
-    entryComponents: [MappingCirclesComponent, MappingTreeComponent],
+    entryComponents: [MappingCirclesComponent, MappingTreeComponent, MappingFirstPersonComponent],
     changeDetection: ChangeDetectionStrategy.OnPush
 
 })
@@ -76,7 +77,7 @@ export class MappingComponent implements OnInit {
 
     ngOnInit() {
         this.subscription = this.route.params.subscribe((params: Params) => {
-             console.log(params["layout"]);
+            console.log(params["layout"]);
             this.layout = params["layout"]
 
             switch (this.layout) {
@@ -85,6 +86,9 @@ export class MappingComponent implements OnInit {
                     break;
                 case "people": // Views.Tree:
                     this.componentFactory = this.componentFactoryResolver.resolveComponentFactory(MappingTreeComponent)
+                    break;
+                case "me":
+                    this.componentFactory = this.componentFactoryResolver.resolveComponentFactory(MappingFirstPersonComponent)
                     break
                 default:
                     throw new Error("This view is not recognized");
