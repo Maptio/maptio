5429ce059fee344662386334b92548530f7edbdc
Safiyya <safiyya.babio@gmail.com>
2017-09-14 00:14:56 +0300

    styling

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -117,10 +117,7 @@ export class LoginComponent implements OnInit {
                     this.loginErrorMessage = "We don't know that email."
                 }
             })
-
-
         }
-
     }
 
 
