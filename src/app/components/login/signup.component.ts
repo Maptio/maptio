575ac22786b94ea90b37f7335c88d3e4fc5dd31c
Safiyya <safiyya.babio@gmail.com>
2017-09-14 12:40:23 +0300

    refacot signup form and add loading spinner

--- src/app/components/login/signup.component.ts
+++ src/app/components/login/signup.component.ts
@@ -1,3 +1,5 @@
+import { LoaderService } from './../../shared/services/http/loader.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Router } from "@angular/router";
 import { UserFactory } from "./../../shared/services/user.factory";
 import { Auth } from "./../../shared/services/auth/auth.service";
@@ -12,10 +14,9 @@ import { User } from "../../shared/model/user.data";
 export class SignupComponent implements OnInit {
 
     public email: string;
-    public password: string;
     public firstname: string;
     public lastname: string;
-    public isPasswordEmpty: boolean;
+    public isTermsAccepted: boolean;
 
     public isEmailAlreadyExist: boolean
     public isRedirectToActivate: boolean;
@@ -23,54 +24,79 @@ export class SignupComponent implements OnInit {
     public signUpMessageFail: string;
     public userToken: string;
 
+    public signupForm: FormGroup;
 
-    constructor(private auth: Auth, private router: Router, private userFactory: UserFactory) { }
+    constructor(private auth: Auth, private router: Router, private userFactory: UserFactory, private loader: LoaderService) {
+        this.signupForm = new FormGroup({
+            "firstname": new FormControl(this.firstname, [
+                Validators.required,
+                Validators.minLength(2)
+            ]),
+            "lastname": new FormControl(this.lastname, [
+                Validators.required,
+                Validators.minLength(2)
+            ]),
+            "email": new FormControl(this.email, [
+                Validators.required
+            ]),
+            "isTermsAccepted": new FormControl(this.isTermsAccepted, [
+                Validators.requiredTrue
+            ])
+        });
+
+    }
 
     ngOnInit() { }
 
-    passwordChange() {
-        // Browser bug similar to https://github.com/angular/angular.js/issues/15985
-        // so i have to check that password after  submit
-        this.isPasswordEmpty = (this.password === "")
-    }
+    createAccount() {
+        this.isConfirmationEmailSent = false;
+        this.isRedirectToActivate = false;
+        this.isEmailAlreadyExist = false;
+        this.signUpMessageFail = "";
+        this.loader.show();
 
-    createAccount(firstname: string, lastname: string, email: string) {
+        if (this.signupForm.dirty && this.signupForm.valid) {
 
-        Promise.all([this.isEmailExist(email), this.isActivationPending(email)]).then(([isEmailExist, { isActivationPending, userToken }]) => {
-            if (isEmailExist) {
-                if (isActivationPending) {
-                    // account is created but still needs activation => redirect to activation page
-                    this.userToken = userToken;
-                    this.isRedirectToActivate = true;
+            let email = this.signupForm.controls["email"].value
+            let firstname = this.signupForm.controls["firstname"].value
+            let lastname = this.signupForm.controls["lastname"].value
+
+            Promise.all([this.isEmailExist(email), this.isActivationPending(email)]).then(([isEmailExist, { isActivationPending, userToken }]) => {
+                if (isEmailExist) {
+                    if (isActivationPending) {
+                        // account is created but still needs activation => redirect to activation page
+                        this.userToken = userToken;
+                        this.isRedirectToActivate = true;
+                    }
+                    else {
+                        // account is created and is already activated => this user should login
+                        this.isEmailAlreadyExist = true;
+                    }
                 }
                 else {
-                    // account is created and is already activated => this user should login
-                    this.isEmailAlreadyExist = true;
+                    // no matching email => create user
+                    this.auth.createUser(email, firstname, lastname, true)
+                        .then((user: User) => {
+                            return user;
+                        })
+                        .then((user: User) => {
+                            console.log("create user", user)
+                            return this.userFactory.create(user)
+                        })
+                        .then((user: User) => {
+                            this.isConfirmationEmailSent = true;
+                        })
+                        .catch((error: Error) => {
+                            this.signUpMessageFail = `We are having issues creating your account! Please email us at support@maptio.com and we'll help you out. `
+                        })
                 }
-            }
-            else {
-                // no matching email => create user
-                this.auth.createUser(email, firstname, lastname, true)
-                    .then((user: User) => {
-                        return user;
-                    })
-                    .then((user: User) => {
-                        console.log("create user", user)
-                        return this.userFactory.create(user)
-                    })
-                    .then((user: User) => {
-                        this.isConfirmationEmailSent = true;
-                        // this.signUpMessageOK = `We have sent a confirmation email to ${user.email}`
-                    })
-                    .catch((error: Error) => {
-                        this.signUpMessageFail = `We are having issues creating your account! Please email us at support@maptio.com and we'll help you out. `
-                    })
-            }
-        })
+            }).then(() => { this.loader.hide(); })
+        }
     }
 
     isEmailExist(email: string): Promise<boolean> {
         return this.userFactory.getAll(email).then((matches: User[]) => {
+            console.log(matches, matches.length)
             if (matches.length === 1) {
                 // this.isEmailAlreadyExist = true;
                 return true;
@@ -86,16 +112,24 @@ export class SignupComponent implements OnInit {
     isActivationPending(email: string) {
         return this.userFactory.getAll(email)
             .then((matches: User[]) => {
+                if (!matches || matches.length === 0) {
+                    return;
+                }
                 if (matches.length === 1) {
                     return matches[0].user_id;
                 }
-                else {
+                if (matches.length > 1) {
                     this.signUpMessageFail = `Several users are registered with this email! Please email us at support@maptio.com and we'll help you out. `
                     throw new Error(this.signUpMessageFail);
                 }
+
             })
             .then((userId: string) => {
+                if (!userId) {
+                    return Promise.resolve({ isActivationPending: false, userToken: null })
+                }
                 return this.auth.isActivationPending(userId).then((isActivationPending: boolean) => {
+                    console.log(isActivationPending)
                     if (isActivationPending) {
                         return this.auth.generateUserToken(userId, email).then(token => {
                             return { isActivationPending, userToken: token }
