44d11ec7252624dd1fcee146adf039783dd3bd4d
Safiyya <safiyya.babio@gmail.com>
2017-09-15 22:59:54 +0300

    cosmetic changes

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -10,8 +10,8 @@ export class FooterComponent implements OnInit {
 
     public TOS_URL: string = "https://termsfeed.com/terms-conditions/f0e548940bde8842b1fb58637ae048c0"
     public PRIVACY_URL: string = "https://termsfeed.com/privacy-policy/61f888ebea93b0029582b88a7be1e1e3"
-    public BLOG_URL:string = "https://medium.com/maptio";
-    
+    public BLOG_URL: string = "https://medium.com/maptio";
+
     constructor() { }
 
     ngOnInit() { }
