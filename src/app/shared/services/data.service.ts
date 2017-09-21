8a892f2525d9fbd50f030105fd738021b62545a9
Safiyya <safiyya.babio@gmail.com>
2017-09-21 18:22:52 +0300

    remove unused code

--- src/app/shared/services/data.service.ts
+++ src/app/shared/services/data.service.ts
@@ -25,10 +25,10 @@ export class DataService {
         return this._data$.asObservable();
     }
 
-    fetch(url: string): Promise<any> {
-        return this.http.get(url)
-            .toPromise()
-            .then(response => response.json())
-            .catch(this.errorService.handleError);
-    }
+    // fetch(url: string): Promise<any> {
+    //     return this.http.get(url)
+    //         .toPromise()
+    //         .then(response => response.json())
+    //         .catch(this.errorService.handleError);
+    // }
 }
