2fb186a7fbd26c845fa0c3b53298cbce3352d093
Safiyya <safiyya.babio@gmail.com>
2017-09-29 15:28:22 +0300

    tests for footer.component.ts

---
+++ src/app/components/footer/footer.component.spec.ts
@@ -0,0 +1,35 @@
+import { environment } from "./../../../environment/environment";
+import { ComponentFixture, async, TestBed } from "@angular/core/testing";
+import { FooterComponent } from "./footer.component";
+
+describe("footer.component.ts", () => {
+
+    let component: FooterComponent;
+    let target: ComponentFixture<FooterComponent>;
+
+    beforeEach(async(() => {
+
+        TestBed.configureTestingModule({
+            declarations: [FooterComponent],
+            schemas: [FooterComponent]
+        }).overrideComponent(FooterComponent, {
+            set: {
+                providers: [
+                ]
+            }
+        }).compileComponents();
+    }));
+
+    beforeEach(() => {
+        target = TestBed.createComponent(FooterComponent);
+
+        component = target.componentInstance;
+    });
+
+    it("should gets correct links ", () => {
+        expect(component.TOS_URL).toBe(environment.TOS_URL);
+        expect(component.PRIVACY_URL).toBe(environment.PRIVACY_URL);
+        expect(component.BLOG_URL).toBe(environment.BLOG_URL);
+    });
+    
+});
