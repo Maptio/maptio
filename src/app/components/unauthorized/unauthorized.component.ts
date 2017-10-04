dde492ccf104753340d4708f3771d64b138f5078
Safiyya <safiyya.babio@gmail.com>
2017-10-04 20:17:09 +0300

    #147

--- src/app/components/unauthorized/unauthorized.component.ts
+++ src/app/components/unauthorized/unauthorized.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from "@angular/core";
 @Component({
     selector: "unauthorized",
     template: `
-    <h4 class="m-3">We don't think you should be allowed here. You disagree ? Email us support@maptio.com</h4>
+    <h4 class="m-3">You don't have permission to view this. Problems? Email us support@maptio.com</h4>
     `
 })
 export class UnauthorizedComponent implements OnInit {
