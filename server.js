52ef968537e07a9ac1e7378ebfdb097475861443
Safiyya <safiyya.babio@gmail.com>
2017-11-12 17:09:42 +0200

    adds fullstory to CSP

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ if (!isDevelopment) {
         'maxcdn.bootstrapcdn.com', 'cdnjs.cloudflare.com', 'cdn.auth0.com', 'api.mixpanel.com',
         'cdn.mxpnl.com', 'cdn4.mxpnl.com',
         'www.google-analytics.com', 'mixpanel.com', 'widget.intercom.io', 'https://app.intercom.io',
-        'https://js.intercomcdn.com', 'https://fullstory.com', 'https://rs.fullstory.com'],
+        'https://js.intercomcdn.com', 'https://fullstory.com'],
       fontSrc: ['maxcdn.bootstrapcdn.com', 'cdn.mixpnl.com', 'https://js.intercomcdn.com'],
       connectSrc: ["'self'", 'api.mixpanel.com', 'api.cloudinary.com', 'circlemapping.auth0.com', 'www.google-analytics.com', 'mixpanel.com', 'https://api.intercom.io', 'https://api-iam.intercom.io',
         'https://api-ping.intercom.io',
@@ -72,7 +72,8 @@ if (!isDevelopment) {
         'wss://nexus-websocket-a.intercom.io',
         'wss://nexus-websocket-b.intercom.io',
         'https://uploads.intercomcdn.com',
-        'https://uploads.intercomusercontent.com',
+        'https://uploads.intercomusercontent.com', 
+        'https://rs.fullstory.com'
       ],
       childSrc: [
         'https://share.intercom.io',
