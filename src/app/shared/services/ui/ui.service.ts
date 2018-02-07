8fb761449ee7cdbdbca399beea2c8d98147ffe96
Safiyya <safiyya.babio@gmail.com>
2018-02-07 13:31:32 +0200

    adding scroll to tooltip

--- src/app/shared/services/ui/ui.service.ts
+++ src/app/shared/services/ui/ui.service.ts
@@ -1,5 +1,5 @@
-import { Injectable } from "@angular/core"
-import { D3Service, D3, Selection, BaseType } from "d3-ng2-service"
+import { Injectable } from "@angular/core";
+import { D3Service, D3, Selection, BaseType } from "d3-ng2-service";
 import "rxjs/add/operator/map";
 import "rxjs/add/operator/toPromise";
 import { Tag } from "../../model/tag.data";
@@ -10,120 +10,192 @@ import { MarkdownService } from "angular2-markdown";
 
 @Injectable()
 export class UIService {
+  private d3: D3;
 
-    private d3: D3;
+  // private tooltip$: Subject<[string, Initiative, Initiative]>
 
-    // private tooltip$: Subject<[string, Initiative, Initiative]>
+  constructor(d3Service: D3Service, private markdown: MarkdownService) {
+    this.d3 = d3Service.getD3();
+    // this.tooltip$ = new Subject<[string, Initiative, Initiative]>();
+  }
 
-    constructor(d3Service: D3Service,
-        private markdown: MarkdownService) {
-        this.d3 = d3Service.getD3();
-        // this.tooltip$ = new Subject<[string, Initiative, Initiative]>();
-    }
+  getCircularPath(radius: number, centerX: number, centerY: number) {
+    if (radius === undefined || centerX === undefined || centerY === undefined)
+      throw new Error(
+        "Cannot defined circular path as a parameter is missing."
+      );
 
+    let rx = -radius;
+    let ry = -radius;
+    return (
+      "m " +
+      centerX +
+      ", " +
+      centerY +
+      " a " +
+      rx +
+      "," +
+      ry +
+      " 1 1,1 " +
+      radius * 2 +
+      ",0 a -" +
+      radius +
+      ",-" +
+      radius +
+      " 1 1,1 -" +
+      radius * 2 +
+      ",0"
+    );
+  }
 
-    getCircularPath(radius: number, centerX: number, centerY: number) {
-        if (radius === undefined || centerX === undefined || centerY === undefined)
-            throw new Error("Cannot defined circular path as a parameter is missing.");
+  public clean() {
+    this.d3
+      .select("svg")
+      .selectAll("*")
+      .remove();
+  }
 
-        let rx = -radius;
-        let ry = -radius;
-        return "m " + centerX + ", " + centerY + " a " + rx + "," + ry + " 1 1,1 " + radius * 2 + ",0 a -" + radius + ",-" + radius + " 1 1,1 -" + radius * 2 + ",0"
-    }
+  wrap(
+    text: Selection<BaseType, {}, HTMLElement, any>,
+    actualText: string,
+    tags: Tag[],
+    width: number,
+    tagLineHeightRatio?: number
+  ) {
+    let d3 = this.d3;
 
-    public clean() {
-        this.d3.select("svg").selectAll("*").remove();
-    }
+    text.each(function() {
+      let text = d3.select(this),
+        words = actualText ? actualText.split(/\s+/).reverse() : [],
+        word: any,
+        line: any[] = [],
+        lineNumber = 0,
+        lineHeight = 1.1, // ems
+        y = text.attr("y"),
+        x = text.attr("x"),
+        dy = parseFloat(text.attr("dy")),
+        tspan = text
+          .text(null)
+          .append("tspan")
+          .attr("x", x)
+          .attr("y", y)
+          .attr("dy", dy + "em");
+      while ((word = words.pop())) {
+        line.push(word);
+        tspan.text(line.join(" "));
+        let node: SVGTSpanElement = <SVGTSpanElement>tspan.node();
+        let hasGreaterWidth = node.getComputedTextLength() > width;
+        if (hasGreaterWidth) {
+          line.pop();
+          tspan.text(line.join(" "));
+          line = [word];
+          tspan = text
+            .append("tspan")
+            .attr("x", x)
+            .attr("y", y)
+            .attr("dy", ++lineNumber * lineHeight + dy + "em")
+            .text(word);
+        }
+      }
 
-    wrap(text: Selection<BaseType, {}, HTMLElement, any>, actualText: string, tags: Tag[], width: number, tagLineHeightRatio?: number) {
-        let d3 = this.d3;
-
+      tags.forEach((tag: Tag, index: number) => {
         text
-            .each(function () {
-                let text = d3.select(this),
-                    words = actualText ? actualText.split(/\s+/).reverse() : [],
-                    word: any,
-                    line: any[] = [],
-                    lineNumber = 0,
-                    lineHeight = 1.1, // ems
-                    y = text.attr("y"),
-                    x = text.attr("x"),
-                    dy = parseFloat(text.attr("dy")),
-                    tspan = text.text(null).append("tspan").attr("x", x).attr("y", y).attr("dy", dy + "em");
-                while (word = words.pop()) {
-                    line.push(word);
-                    tspan.text(line.join(" "));
-                    let node: SVGTSpanElement = <SVGTSpanElement>tspan.node();
-                    let hasGreaterWidth = node.getComputedTextLength() > width;
-                    if (hasGreaterWidth) {
-                        line.pop();
-                        tspan.text(line.join(" "));
-                        line = [word];
-                        tspan = text.append("tspan").attr("x", x).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
-                    }
-                }
+          .append("tspan")
+          .attr("x", x)
+          .attr("y", y)
+          .attr(
+            "dy",
+            (lineNumber + 1) * (lineHeight * (tagLineHeightRatio || 1)) +
+              dy +
+              "em"
+          )
+          .attr("dx", index * 20)
+          .attr("class", "dot-tags")
+          .attr("fill", tag.color)
+          .html("&#xf02b");
+      });
+    });
+  }
 
-                tags.forEach((tag: Tag, index: number) => {
-                    text.append("tspan")
-                        .attr("x", x)
-                        .attr("y", y)
-                        .attr("dy", (lineNumber + 1) * (lineHeight * (tagLineHeightRatio || 1)) + dy + "em")
-                        .attr("dx", index * 20)
-                        .attr("class", "dot-tags")
-                        .attr("fill", tag.color)
-                        .html("&#xf02b");
-                })
+  filter(
+    selectedTags: any[],
+    unselectedTags: any[],
+    selection: any[]
+  ): boolean {
+    return _.isEmpty(selectedTags) // all tags are unselected by default
+      ? true
+      : _.isEmpty(selection) // the circle doesnt have any tags
+        ? false
+        : _.intersection(selectedTags.map(t => t.shortid), selection).length ===
+          0
+          ? false
+          : true;
+  }
 
-
-            });
-    }
-
-    filter(selectedTags: any[], unselectedTags: any[], selection: any[]): boolean {
-        return _.isEmpty(selectedTags) // all tags are unselected by default
-            ? true
-            : _.isEmpty(selection) // the circle doesnt have any tags
-                ? false
-                : _.intersection(selectedTags.map(t => t.shortid), selection).length === 0
-                    ? false
-                    : true;
-    }
-
-    getTooltipHTML(initiative: Initiative): string {
-        let tagsSpan = initiative.tags
-            ? initiative.tags.map((tag: Tag) => `
-        <li><a class="btn btn-sm btn-secondary borderless mr-1 tag" style="color:${tag.color}">
-                <i class="fa fa-tag mr-1" aria-hidden="true" style="color:${tag.color}"></i>${tag.name}
+  getTooltipHTML(initiative: Initiative): string {
+    let tagsSpan = initiative.tags
+      ? initiative.tags
+          .map(
+            (tag: Tag) => `
+        <li><a class="btn btn-sm btn-secondary borderless mr-1 tag" style="color:${
+          tag.color
+        }">
+                <i class="fa fa-tag mr-1" aria-hidden="true" style="color:${
+                  tag.color
+                }"></i>${tag.name}
             </a></li>
-        `).join("")
-            : "";
+        `
+          )
+          .join("")
+      : "";
 
-        let accountableImg = initiative.accountable
-            ? `<button class="open-summary btn btn-link pl-0" data-shortid="${initiative.accountable.shortid}" data-slug="${initiative.accountable.getSlug()}">
-                    <img src="${initiative.accountable.picture}" width="30" height="30" class="rounded-circle mr-1">${initiative.accountable.name}
+    let accountableImg = initiative.accountable
+      ? `<button class="open-summary btn btn-link pl-0" data-shortid="${
+          initiative.accountable.shortid
+        }" data-slug="${initiative.accountable.getSlug()}">
+                    <img src="${
+                      initiative.accountable.picture
+                    }" width="30" height="30" class="rounded-circle mr-1">${
+          initiative.accountable.name
+        }
                 </button>`
-            : "<a ></a>";
+      : "<a ></a>";
 
-        let helpersImg = initiative.helpers
-            ? initiative.helpers.map((helper: Helper) =>
-                `
-                <button class="mr-1 open-summary btn btn-link p-0" data-shortid="${helper.shortid}" data-slug="${helper.getSlug()}" >
-                    <img src="${helper.picture}" width="15" height="15" class="rounded-circle mr-1"><small>${helper.name}</small>
+    let helpersImg = initiative.helpers
+      ? initiative.helpers
+          .map(
+            (helper: Helper) =>
+              `
+                <button class="mr-1 open-summary btn btn-link p-0" data-shortid="${
+                  helper.shortid
+                }" data-slug="${helper.getSlug()}" >
+                    <img src="${
+                      helper.picture
+                    }" width="15" height="15" class="rounded-circle mr-1"><small>${
+                helper.name
+              }</small>
                 </button>`
-            ).join("")
-            : "";
+          )
+          .join("")
+      : "";
 
-        return `
-                <h6 class="mb-1 lead"><button class="btn btn-link lead open-initiative" id="${initiative.id}">${initiative.name}</button></h6>
+    return `
+        <div class="content">
+                <h6 class="mb-1 lead"><button class="btn btn-link lead open-initiative" id="${
+                  initiative.id
+                }">${initiative.name}</button></h6>
                 <div class="mb-0 p-0 ml-0">${accountableImg}</div>
                 <div class="row pl-3 mb-2 d-flex justify-content-start" >${helpersImg}</div>
                 <ul class="tags small"> ${tagsSpan}</ul>
-                <small>${this.markdown.compile(initiative.description || "")}</small>
+                <small>${this.markdown.compile(
+                  initiative.description || ""
+                )}</small>
                 </textPath>
-                `
-            ;
-    }
+                </div>
+                `;
+  }
 
-    /*
+  /*
     adjustLabels(textNodes: Selection<BaseType, HierarchyCircularNode<{}>, BaseType, {}>, k: number) {
         let d3 = this.d3;
         textNodes
@@ -152,6 +224,4 @@ export class UIService {
             });
     }
 */
-
-
 }
