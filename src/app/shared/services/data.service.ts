a2520450b163c5be02c2bd2f56b6d84f2749f93c
Safiyya <safiyya.babio@gmail.com>
2018-02-17 12:34:04 +0200

    add tests for uri.service.ts

--- src/app/shared/services/data.service.ts
+++ src/app/shared/services/data.service.ts
@@ -34,23 +34,3 @@ export class DataService {
 
 
 
-export class URIService {
-    parseFragment(fragment: string): Map<string, string> {
-        if (!fragment) return new Map<string, string>();
-        let query = new Map<string, string>();
-        let pairs = (fragment[0] === "#" ? fragment.substr(1) : fragment).split("&");
-        for (let i = 0; i < pairs.length; i++) {
-            let pair = pairs[i].split("=");
-            query.set(decodeURIComponent(pair[0]), pair[1] ? decodeURIComponent(pair[1]) : undefined);
-        }
-        return query;
-    }
-
-    buildFragment(data: Map<string, string>): string {
-        let fragment = "";
-        data.forEach((v, k, map) => {
-            if (k) fragment += `${k}=${v}&`
-        });
-        return fragment.slice(0, fragment.length - 1);
-    }
-}
---
+++ src/app/shared/services/uri.service.spec.ts
@@ -0,0 +1,64 @@
+import { TestBed, inject } from "@angular/core/testing";
+import { URIService } from "./uri.service";
+
+fdescribe("uri.service.ts", () => {
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [
+                URIService
+            ]
+        });
+    })
+
+    describe("parseFragment", () => {
+        it("should return correct Map when fragment is precedeed by #", inject([URIService], (target: URIService) => {
+            let fragment = "#x=abc&y=123&array=col1,col2,col3"
+            let actual = target.parseFragment(fragment)
+            expect(actual.size).toBe(3);
+            expect(actual.get("x")).toBe("abc")
+            expect(actual.get("y")).toBe("123")
+            expect(actual.get("array")).toBe("col1,col2,col3")
+        }));
+
+        it("should return correct Map when fragment is not precedeed by #", inject([URIService], (target: URIService) => {
+            let fragment = "x=abc&y=123&array=col1,col2,col3"
+            let actual = target.parseFragment(fragment)
+            expect(actual.size).toBe(3);
+            expect(actual.get("x")).toBe("abc")
+            expect(actual.get("y")).toBe("123")
+            expect(actual.get("array")).toBe("col1,col2,col3")
+        }));
+
+        it("should return empty map when fragment is empty", inject([URIService], (target: URIService) => {
+            let fragment = ""
+            let actual = target.parseFragment(fragment)
+            expect(actual.size).toBe(0);
+        }));
+
+        it("should return empty map when fragment is null", inject([URIService], (target: URIService) => {
+            let fragment = null
+            let actual = target.parseFragment(fragment)
+            expect(actual.size).toBe(0);
+        }));
+
+        it("should return empty map when fragment is undefined", inject([URIService], (target: URIService) => {
+            let fragment = undefined
+            let actual = target.parseFragment(fragment)
+            expect(actual.size).toBe(0);
+        }));
+    });
+
+
+
+    describe("buildFragment", () => {
+        it("should return correct fragment", inject([URIService], (target: URIService) => {
+            let fragmentMap = new Map<string, string>();
+            fragmentMap.set("x", "abc");
+            fragmentMap.set("y", "123");
+            fragmentMap.set("array", "col1,col2,col3");
+            let actual = target.buildFragment(fragmentMap)
+            expect(actual).toBe("x=abc&y=123&array=col1,col2,col3");
+        }));
+    })
+}
