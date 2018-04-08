2a178b62254017b05367b22a968fc075b33902e0
Safiyya <safiyya.babio@gmail.com>
2018-04-08 16:58:42 +0300

    alignement

--- src/app/components/unauthorized/unauthorized.component.ts
+++ src/app/components/unauthorized/unauthorized.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from "@angular/core";
 @Component({
     selector: "unauthorized",
     template: `
-    <h4 class="m-3">You don't have permission to view this. Problems? Email us support@maptio.com</h4>
+    <h5 class="m-3">You don't have permission to view this. Problems? Email us <b ><a class="text-success" href="mailto:support@maptio.com">support@maptio.com</a></b></h5>
     `
 })
 export class UnauthorizedComponent implements OnInit {
