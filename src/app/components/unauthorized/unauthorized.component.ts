609ff79a7b9426f7b5b8846f37d61a76b440fc2a
Safiyya <safiyya.babio@gmail.com>
2018-04-10 00:39:16 +0300

    better help and unauthorized pages

--- src/app/components/unauthorized/unauthorized.component.ts
+++ src/app/components/unauthorized/unauthorized.component.ts
@@ -2,9 +2,7 @@ import { Component, OnInit } from "@angular/core";
 
 @Component({
     selector: "unauthorized",
-    template: `
-    <h5 class="m-3">You don't have permission to view this. Problems? Email us <b ><a class="text-success" href="mailto:support@maptio.com">support@maptio.com</a></b></h5>
-    `
+    templateUrl: "./unauthorized.component.html"
 })
 export class UnauthorizedComponent implements OnInit {
     constructor() { }
