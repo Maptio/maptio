7fd822dedd2baba3a73407bbcd2c427f62685298
Safiyya <safiyya.babio@gmail.com>
2018-11-06 17:42:48 +0200

    fixing build

--- src/app/components/unauthorized/unauthorized.component.ts
+++ src/app/components/unauthorized/unauthorized.component.ts
@@ -7,7 +7,7 @@ import { Router } from "../../../../node_modules/@angular/router";
     templateUrl: "./unauthorized.component.html"
 })
 export class UnauthorizedComponent implements OnInit {
-    private timeToRedirect:number = 5;
+    public timeToRedirect:number = 5;
     private subscription:Subscription;
     constructor(private cd:ChangeDetectorRef, private roouter:Router) { }
 
