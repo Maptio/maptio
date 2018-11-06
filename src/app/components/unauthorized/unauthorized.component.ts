33c4561a8d3ec48ba4cde1d268b6c91f19d1f559
Safiyya <safiyya.babio@gmail.com>
2018-11-06 17:31:57 +0200

    unauthorized redirect to homepage

--- src/app/components/unauthorized/unauthorized.component.ts
+++ src/app/components/unauthorized/unauthorized.component.ts
@@ -1,11 +1,29 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ChangeDetectorRef } from "@angular/core";
+import { Subscription, Observable } from "../../../../node_modules/rxjs";
+import { Router } from "../../../../node_modules/@angular/router";
 
 @Component({
     selector: "unauthorized",
     templateUrl: "./unauthorized.component.html"
 })
 export class UnauthorizedComponent implements OnInit {
-    constructor() { }
+    private timeToRedirect:number = 5;
+    private subscription:Subscription;
+    constructor(private cd:ChangeDetectorRef, private roouter:Router) { }
 
-    ngOnInit() { }
+    ngOnInit() { 
+        this.subscription = Observable.timer(1000, 1000).subscribe(i =>{
+            this.timeToRedirect -= 1;
+            this.cd.markForCheck();
+
+            if(this.timeToRedirect ===0){
+                this.roouter.navigateByUrl("/home");
+            }
+        })
+
+    }
+
+    ngOnDestroy(): void {
+      if(this.subscription) this.subscription.unsubscribe();
+    }
 }
