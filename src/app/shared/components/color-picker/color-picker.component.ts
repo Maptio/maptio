fb7d5cb373228ae0895e3fdea763e1b1bdb56240
Safiyya <safiyya.babio@gmail.com>
2019-01-14 22:33:25 +0200

    remove text size settings

--- src/app/shared/components/color-picker/color-picker.component.ts
+++ src/app/shared/components/color-picker/color-picker.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { environment } from '../../../../environment/environment';
 
 @Component({
     selector: 'common-color-picker',
@@ -8,19 +9,27 @@ import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 export class ColorPickerComponent implements OnInit {
 
     @Input("color") color: string;
+    @Input("label") label:string;
     @Input("default") defaultColor: string;
     @Input("isMinimal") isMinimal: boolean;
     @Output("change") changeColor: EventEmitter<string> = new EventEmitter<string>();
+
+    DEFAULT_COLOR :string = environment.DEFAULT_MAP_TEXT_COLOR;
 
     constructor() { }
 
     ngOnInit(): void { }
 
-    change(color: string) {
+    pickColor(color: string) {
         this.changeColor.emit(color);
     }
 
+    typeColor(e : {input: string, value: number | string, color: string}) {
+        console.log(e)
+        this.pickColor(e.color);
+    }
+
     reset(){
-        this.change(this.defaultColor);
+        this.pickColor(this.defaultColor);
     }
 }
