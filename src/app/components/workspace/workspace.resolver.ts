c3593a7a09b6e54f31837b64785a52874dffc14b
Safiyya <safiyya.babio@gmail.com>
2019-01-21 17:17:15 +0200

    workspace resolver with RxJS6

--- src/app/components/workspace/workspace.resolver.ts
+++ src/app/components/workspace/workspace.resolver.ts
@@ -11,6 +11,8 @@ import { UserFactory } from "../../shared/services/user.factory";
 import { SelectableTag } from "../../shared/model/tag.data";
 import { Observable } from "rxjs/Observable";
 import { UserService } from "../../shared/services/user/user.service";
+import { from } from "rxjs";
+import { map, flatMap } from "rxjs/operators"
 
 @Injectable()
 export class WorkspaceComponentResolver implements Resolve<{ dataset: DataSet, team: Team, members: User[], user: User }> {
@@ -21,43 +23,41 @@ export class WorkspaceComponentResolver implements Resolve<{ dataset: DataSet, t
     resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<{ dataset: DataSet, team: Team, members: User[], user: User }> {
         let datasetId = <string>route.params["mapid"];
 
-        // return Observable.forkJoin(
-        return Observable.fromPromise(
-            this.datasetFactory.get(datasetId)
-                .then((dataset: DataSet) => {
-
+        return from(this.datasetFactory.get(datasetId))
+            .pipe(
+                map((dataset: DataSet) => {
                     dataset.tags = dataset.tags.map(t => new SelectableTag(t)).map(t => { t.isSelected = false; return t });
                     return dataset;
-                })
-                .then((dataset: DataSet) => {
+                }),
+                flatMap((dataset: DataSet) => {
                     return this.teamFactory.get(dataset.initiative.team_id)
                         .then(
                             t => {
                                 return { dataset: dataset, team: t }
                             })
-                })
-                .then(dt => {
+                }),
+                flatMap(dt => {
                     return Promise.all([this.userService.getUsersInfo(dt.team.members), this.userFactory.getUsers(dt.team.members.map(m => m.user_id))])
                         .then(([auth0Users, databaseUsers]: [User[], User[]]) => {
                             return databaseUsers.map(u => {
                                 u.picture = auth0Users.find(du => du.user_id === u.user_id) ? auth0Users.find(du => du.user_id === u.user_id).picture : u.picture;
-                               return u;
+                                return u;
                             })
                         })
                         .then(members => compact(members))
                         .then(members => sortBy(members, m => m.name))
                         .then(members => { return { dataset: dt.dataset, team: dt.team, members: members } })
 
-                })
-                .then(dt => {
+
+                }),
+                flatMap(dt => {
                     return this.auth.getUser().first().toPromise().then(u => { return { user: u, data: dt } })
-                })
-                .then(data => {
+
+                }),
+                map(data => {
                     return { dataset: data.data.dataset, team: data.data.team, members: data.data.members, user: data.user }
-
-                })
-        )
-
+                }
+                )
+            )
     }
-
 }
