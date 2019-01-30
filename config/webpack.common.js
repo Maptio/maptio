6799872e3db39de4debea1ad12805c906ee0459a
Safiyya <safiyya.babio@gmail.com>
2019-01-30 17:58:01 +0200

    trying small JS payloads

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -40,17 +40,39 @@ of lodash in @exalif/ngx-breadcrums
     }
   },
 
+  // optimization: {
+  //   splitChunks: {
+  //     cacheGroups: {
+  //       vendor: {
+  //         name: 'vendor',
+  //         test: /\/node_modules\//,
+  //         chunks: 'all',
+  //         priority: 0,
+  //         enforce: true,
+  //       },
+  //     }
+  //   },
+  // },
+
   optimization: {
+    runtimeChunk: 'single',
     splitChunks: {
+      chunks: 'all',
+      maxInitialRequests: Infinity,
+      minSize: 0,
       cacheGroups: {
         vendor: {
-          name: 'vendor',
-          test: /\/node_modules\//,
-          chunks: 'all',
-          priority: 0,
-          enforce: true,
+          test: /[\\/]node_modules[\\/]/,
+          name(module) {
+            // get the name. E.g. node_modules/packageName/not/this/part.js
+            // or node_modules/packageName
+            const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+
+            // npm package names are URL-safe, but some servers don't like @ symbols
+            return `npm.${packageName.replace('@', '')}`;
+          },
         },
-      }
+      },
     },
   },
 
