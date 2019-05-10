68eca16aabe314156304788e280ea40b1e20d2a5
Safiyya <safiyya.babio@gmail.com>
2019-05-10 19:26:54 +0300

    server limit iz 2mb

--- server.ts
+++ server.ts
@@ -102,7 +102,7 @@ if (!isDevelopment) {
 let cache = apicache.middleware
 // app.use(cache('5 seconds'))
 app.use(bodyParser.text({ type: "text/html", limit: '5mb' }))
-app.use(bodyParser.json({ limit: '5mb' }));
+app.use(bodyParser.json({ limit: '2mb' }));
 app.use(sslRedirect());
 app.use(compression())
 // app.use(jwtCheck.unless({ path: ['/','/api/v1/mail/confirm', "/api/v1/jwt/encode", "/api/v1/jwt/decode"] }));
