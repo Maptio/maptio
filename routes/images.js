cddd87dd602e6a7ddb6d3afcaf6c96080243fdeb
Safiyya <safiyya.babio@gmail.com>
2019-05-10 19:26:10 +0300

    updating cloudinary

--- routes/images.js
+++ routes/images.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var cloudinary = require('cloudinary');
+var cloudinary = require('cloudinary').v2;
 var fs = require('fs');
 var path = require('path');
 
@@ -27,17 +27,37 @@ router.post('/upload/:mapid', function (req, res, next) {
     var base64String = Buffer.from(svgString).toString("base64");
     cloudinary.uploader.upload(
         `data:image/svg+xml;base64,${base64String}`,
-        function (result) { res.json(result) },
         {
             public_id: `${mapid}/snapshot/${date.toISOString()}`,
             tags: [],
             eager: {
                 background: "#ffffff",
                 quality: "auto:best",
-                format:"png"
+                format: "png"
             }
 
-        });
+        },
+        function (error, result) {
+            if (error) {
+                res.send(error);
+            }
+            res.json(result)
+        }
+    );
+
+    // cloudinary.uploader.upload(
+    //     `data:image/svg+xml;base64,${base64String}`,
+    //     function (result) { res.json(result) },
+    //     {
+    //         public_id: `${mapid}/snapshot/${date.toISOString()}`,
+    //         tags: [],
+    //         eager: {
+    //             background: "#ffffff",
+    //             quality: "auto:best",
+    //             format: "png"
+    //         }
+
+    //     });
 });
 
 
