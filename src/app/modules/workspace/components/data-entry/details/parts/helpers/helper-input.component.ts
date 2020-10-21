42d170ae13f7ec8c8443469efc3cbe1d1593772f
Roman Goj <roman.goj@gmail.com>
2020-10-21 15:03:31 +0100

    Clean up helper input before refactoring

--- src/app/modules/workspace/components/data-entry/details/parts/helpers/helper-input.component.ts
+++ src/app/modules/workspace/components/data-entry/details/parts/helpers/helper-input.component.ts
@@ -1,41 +1,33 @@
-import { Component, OnInit, Input, Output, EventEmitter, ChangeDetectorRef, SimpleChanges } from '@angular/core';
-import { Helper } from '../../../../../../../shared/model/helper.data';
-import { Team } from '../../../../../../../shared/model/team.data';
-import { Role } from '../../../../../../../shared/model/role.data';
+import { Component, OnInit, Input, Output, EventEmitter, ChangeDetectorRef, SimpleChanges } from "@angular/core";
+import { Helper } from "../../../../../../../shared/model/helper.data";
+import { Team } from "../../../../../../../shared/model/team.data";
+import { Role } from "../../../../../../../shared/model/role.data";
 
 @Component({
-    selector: 'initiative-helper-input',
-    templateUrl: './helper-input.component.html',
-    // styleUrls: ['./helper-input.component.css']
+    selector: "initiative-helper-input",
+    templateUrl: "./helper-input.component.html",
+    // styleUrls: ["./helper-input.component.css"]
 })
 export class InitiativeHelperInputComponent implements OnInit {
     @Input("helper") helper: Helper;
     @Input("team") team: Team;
     @Input("summaryUrlRoot") summaryUrlRoot: string;
     @Input("isAuthority") isAuthority: boolean;
-    @Input("isUnauthorized") isUnauthorized:boolean;
+    @Input("isUnauthorized") isUnauthorized: boolean;
 
     @Output("remove") remove: EventEmitter<Helper> = new EventEmitter<Helper>();
-    @Output("save") save :EventEmitter<void> = new EventEmitter<void>();
+    @Output("save") save: EventEmitter<void> = new EventEmitter<void>();
 
-    placeholder:string;
-    cancelClicked:boolean;
-    hasRole:boolean;
-    isEditRoleToggled:boolean;
-    isAddRoleMode = false;
+    cancelClicked: boolean;
+    isPickRoleMode = false;
 
     constructor(private cd: ChangeDetectorRef) { }
 
     ngOnInit(): void { }
 
     ngOnChanges(changes:SimpleChanges){
-        if(changes.helper && changes.helper.currentValue){
-            this.placeholder = `How is ${changes.helper.currentValue.name} helping?`
-            this.hasRole = (changes.helper.currentValue as Helper).roles 
-            && (changes.helper.currentValue as Helper).roles[0] 
-            && !!(changes.helper.currentValue as Helper).roles[0].description;
-            this.isEditRoleToggled = this.hasRole;
-            if(this.isAuthority) this.helper.hasAuthorityPrivileges = true;
+        if (changes.helper && changes.helper.currentValue) {
+            if (this.isAuthority) this.helper.hasAuthorityPrivileges = true;
         }
     }
 
@@ -50,7 +42,7 @@ export class InitiativeHelperInputComponent implements OnInit {
     }
 
     onRolePick(roles: Role[]) {
-        this.isAddRoleMode = false;
+        this.isPickRoleMode = false;
         this.helper.roles = roles;
         this.save.emit();
         this.cd.markForCheck();
