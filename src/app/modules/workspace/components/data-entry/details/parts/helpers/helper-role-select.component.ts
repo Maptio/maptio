6a6af76f5c0cde7a591785269eb6a15ce36660f2
Roman Goj <roman.goj@gmail.com>
2020-10-22 17:01:09 +0100

    Remove more unnecessary commented out code

--- src/app/modules/workspace/components/data-entry/details/parts/helpers/helper-role-select.component.ts
+++ src/app/modules/workspace/components/data-entry/details/parts/helpers/helper-role-select.component.ts
@@ -1,51 +1,25 @@
-// import { Component, OnInit, Input, Output, ChangeDetectorRef, SimpleChanges, EventEmitter } from '@angular/core';
 import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
 
 import { isEqual } from "lodash-es";
 
-// import { Team } from '../../../../../../../shared/model/team.data';
-// import { Helper } from '../../../../../../../shared/model/helper.data';
-// import { Role } from '../../../../../shared/model/role.data';
 import { Role } from "../../../../../../../shared/model/role.data";
-// import { of } from 'rxjs/observable/of';
-// import { Auth } from '../../../../../../../core/authentication/auth.service';
-// import { User } from '../../../../../../../shared/model/user.data';
-// import { Subscription, Subject } from 'rxjs';
 import { RoleLibraryService } from "../../../../../services/role-library.service";
 
 @Component({
     selector: "initiative-helper-role-select",
     templateUrl: "./helper-role-select.component.html",
-    // styleUrls: ['./helpers-select.component.css']
 })
 export class InitiativeHelperRoleSelectComponent implements OnInit {
 
-    // @Input("team") team: Team;
     @Input("roles") roles: Role[];
-    // @Input("user") user: User;
-    // @Input("authority") authority: Helper;
-    // @Input("isEditMode") isEditMode: boolean;
-    // @Input("isUnauthorized") isUnauthorized: boolean;
 
     @Output("pick") pick = new EventEmitter<Array<Role>>();
     @Output("create") create = new EventEmitter<Role>();
 
-    // placeholder: string;
-    // subscription: Subscription;
-    // isLoaded: boolean;
-
-    // constructor(private auth: Auth, private cd: ChangeDetectorRef) { }
     constructor(private roleLibrary: RoleLibraryService) { }
 
     ngOnInit() {
     }
-
-    // isCurrentUserAlredyAdded() {
-    //     if (this.helpers && this.user) {
-    //         return this.helpers.concat([this.authority]).filter(h => !!h).findIndex(h => h.user_id === this.user.user_id) > -1;
-    //     }
-    //     this.cd.markForCheck();
-    // }
 
     onAddingRole(newRole: Role) {
         if (newRole.isLibraryRole()) {
@@ -57,12 +31,6 @@ export class InitiativeHelperRoleSelectComponent implements OnInit {
             this.create.emit(newRole);
         }
     }
-
-    // onAddingCurrentUser() {
-    //     this.onAddingHelper(this.user as Helper)
-    // }
-
-
 
     // /**
     // * Leave a fat arrow in order to fixate the this and be able to use in child component 
