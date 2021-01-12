3e9887689dd4de03dad55ea6a2d8d0bbaccbc5f7
Romek Goj <roman.goj@gmail.com>
2021-01-12 16:02:56 +0000

    Uncomment some new component code and fix errors

--- src/app/modules/workspace/components/data-entry/details/parts/helpers/vacancies-input.component.ts
+++ src/app/modules/workspace/components/data-entry/details/parts/helpers/vacancies-input.component.ts
@@ -9,38 +9,36 @@ import { Role } from "../../../../../../../shared/model/role.data";
     styleUrls: ["./vacancies-input.component.css"],
 })
 export class InitiativeVacanciesInputComponent implements OnInit {
-    // @Input("vacancies") vacancies: Role[];
+    @Input("vacancies") vacancies: Role[];
     // @Input("team") team: Team;
-    // @Input("summaryUrlRoot") summaryUrlRoot: string;
-    // @Input("isUnauthorized") isUnauthorized: boolean;
+    @Input("summaryUrlRoot") summaryUrlRoot: string;
+    @Input("isUnauthorized") isUnauthorized: boolean;
 
     // @Output("remove") remove: EventEmitter<Helper> = new EventEmitter<Helper>();
     // @Output("save") save: EventEmitter<void> = new EventEmitter<void>();
 
-    // cancelClicked: boolean;
-    // isPickRoleMode = false;
-    // isCreateRoleMode = false;
-    // isEditRoleMode = false;
+    cancelClicked: boolean;
+    isPickRoleMode = false;
+    isCreateRoleMode = false;
+    isEditRoleMode = false;
 
-    // newRole: Role;
-    // roleBeingEdited: Role;
+    newRole: Role;
+    roleBeingEdited: Role;
 
     constructor(private cd: ChangeDetectorRef) { }
 
     ngOnInit(): void { }
 
-    ngOnChanges(changes:SimpleChanges){
-        // if (changes.helper && changes.helper.currentValue) {
-        //     if (this.isAuthority) this.helper.hasAuthorityPrivileges = true;
+    ngOnChanges(changes: SimpleChanges){
+        if (changes.vacancies && changes.vacancies.currentValue) {
+            this.isPickRoleMode = false;
 
-        //     this.isPickRoleMode = false;
+            this.isCreateRoleMode = false;
+            this.newRole = undefined;
 
-        //     this.isCreateRoleMode = false;
-        //     this.newRole = undefined;
-
-        //     this.isEditRoleMode = false;
-        //     this.roleBeingEdited = undefined;
-        // }
+            this.isEditRoleMode = false;
+            this.roleBeingEdited = undefined;
+        }
     }
 
     // onRemove() {
@@ -53,34 +51,34 @@ export class InitiativeVacanciesInputComponent implements OnInit {
     //     this.cd.markForCheck();
     // }
 
-    // onPickRole(roles: Role[]) {
-    //     this.isPickRoleMode = false;
+    onPickRole(roles: Role[]) {
+        this.isPickRoleMode = false;
     //     this.vacancies = this.sortRoles(roles);
     //     this.save.emit();
     //     this.cd.markForCheck();
-    // }
+    }
 
-    // onCreateRole(newRole: Role) {
-    //     this.isPickRoleMode = false;
-    //     this.isCreateRoleMode = true;
-    //     this.newRole = newRole;
-    //     this.cd.markForCheck();
-    // }
+    onCreateRole(newRole: Role) {
+        this.isPickRoleMode = false;
+        this.isCreateRoleMode = true;
+        this.newRole = newRole;
+        this.cd.markForCheck();
+    }
 
-    // onCancelCreatingNewRole() {
-    //     this.isCreateRoleMode = false;
-    //     this.newRole = undefined;
-    //     this.cd.markForCheck();
-    // }
+    onCancelCreatingNewRole() {
+        this.isCreateRoleMode = false;
+        this.newRole = undefined;
+        this.cd.markForCheck();
+    }
 
-    // onSaveNewRole() {
-    //     this.vacancies.unshift(this.newRole);
-    //     this.vacancies = this.sortRoles(this.vacancies);
-    //     this.isCreateRoleMode = false;
-    //     this.newRole = undefined;
+    onSaveNewRole() {
+        this.vacancies.unshift(this.newRole);
+        this.vacancies = this.sortRoles(this.vacancies);
+        this.isCreateRoleMode = false;
+        this.newRole = undefined;
     //     this.save.emit();
-    //     this.cd.markForCheck();
-    // }
+        this.cd.markForCheck();
+    }
 
     // onEditRole(role: Role) {
     //     this.roleBeingEdited = role;
@@ -105,32 +103,32 @@ export class InitiativeVacanciesInputComponent implements OnInit {
     //     this.cd.markForCheck();
     // }
 
-    // sortRoles(roles: Role[]): Role[] {
-    //     return roles.sort((a, b) => {
-    //         // Make sure that roles without titles end up at the bottom of the list
-    //         if (!a.hasTitle() && b.hasTitle()) {
-    //             return 1;
-    //         }
+    sortRoles(roles: Role[]): Role[] {
+        return roles.sort((a, b) => {
+            // Make sure that roles without titles end up at the bottom of the list
+            if (!a.hasTitle() && b.hasTitle()) {
+                return 1;
+            }
 
-    //         if (a.hasTitle() && !b.hasTitle()) {
-    //             return -1;
-    //         }
+            if (a.hasTitle() && !b.hasTitle()) {
+                return -1;
+            }
 
-    //         // Sort the remaining roles by title
-    //         if (a.hasTitle() && b.hasTitle()) {
-    //             const aTitle = a.title.toLowerCase();
-    //             const bTitle = b.title.toLowerCase();
+            // Sort the remaining roles by title
+            if (a.hasTitle() && b.hasTitle()) {
+                const aTitle = a.title.toLowerCase();
+                const bTitle = b.title.toLowerCase();
 
-    //             if (aTitle > bTitle) {
-    //                 return 1;
-    //             }
+                if (aTitle > bTitle) {
+                    return 1;
+                }
 
-    //             if (aTitle < bTitle) {
-    //                 return -1;
-    //             }
-    //         }
+                if (aTitle < bTitle) {
+                    return -1;
+                }
+            }
 
-    //         return 0;
-    //     });
-    // }
+            return 0;
+        });
+    }
 }
