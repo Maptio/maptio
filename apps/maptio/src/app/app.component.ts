e8afffb47d44b6a70c7b01689c1283ea640bb02c
Romek Goj <roman.goj@gmail.com>
2021-03-15 15:36:17 +0000

    [WIP] Run d3 circle packing on imported map data

--- apps/maptio/src/app/app.component.ts
+++ apps/maptio/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { hierarchy, pack } from 'd3-hierarchy';
+
 import data from './markhof.data.json';
 
 @Component({
@@ -12,5 +14,34 @@ export class AppComponent {
 
   constructor() {
     console.log(data);
+    console.log(pack);
+
+    const diameter = 1000;
+    const margin = 15;
+    const PADDING_CIRCLE = 20;
+
+    const boo = pack()
+      .size([diameter - margin, diameter - margin])
+      .padding(function () { // eslint-disable-line @typescript-eslint/no-explicit-any
+        return PADDING_CIRCLE;
+      });
+
+    console.log('data before running hierarchy:');
+    console.log(data);
+      
+    const root: any = hierarchy(data) // eslint-disable-line @typescript-eslint/no-explicit-any
+    .sum(function (d) {
+      return (Object.prototype.hasOwnProperty.call(d, 'accountable')? 1 : 0) +
+        (Object.prototype.hasOwnProperty.call(d, 'helpers') ? d.helpers.length : 0) + 1;
+    });
+    // .sort(function (a, b) {
+    //   return b.value - a.value;
+    // });
+
+    console.log('data after running hierarchy:');
+    console.log(root);
+
+    console.log('data after running circle packing:');
+    console.log(boo(root).descendants());
   }
 }
