a94c99f52186234aab41af1fc8be1464d9f553e9
Romek Goj <roman.goj@gmail.com>
2021-03-16 14:18:10 +0000

    [WIP] Show circles with colours and titles

--- apps/maptio/src/app/app.component.ts
+++ apps/maptio/src/app/app.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
 
-import { hierarchy, pack } from 'd3-hierarchy';
+import { hierarchy, pack, HierarchyCircularNode } from 'd3-hierarchy';
 
 import data from './markhof.data.json';
+
+interface Initiative {
+  name: string;
+  colour: string;
+}
 
 @Component({
   selector: 'maptio-root',
@@ -11,6 +16,8 @@ import data from './markhof.data.json';
 })
 export class AppComponent {
   title = 'maptio';
+  circles: HierarchyCircularNode<Initiative>[];
+  math = Math;
 
   constructor() {
     console.log(data);
@@ -20,7 +27,7 @@ export class AppComponent {
     const margin = 15;
     const PADDING_CIRCLE = 20;
 
-    const boo = pack()
+    const boo = pack<Initiative>()
       .size([diameter - margin, diameter - margin])
       .padding(function () { // eslint-disable-line @typescript-eslint/no-explicit-any
         return PADDING_CIRCLE;
@@ -28,20 +35,25 @@ export class AppComponent {
 
     console.log('data before running hierarchy:');
     console.log(data);
-      
+
     const root: any = hierarchy(data) // eslint-disable-line @typescript-eslint/no-explicit-any
-    .sum(function (d) {
-      return (Object.prototype.hasOwnProperty.call(d, 'accountable')? 1 : 0) +
-        (Object.prototype.hasOwnProperty.call(d, 'helpers') ? d.helpers.length : 0) + 1;
-    });
-    // .sort(function (a, b) {
-    //   return b.value - a.value;
-    // });
+      .sum(function (d) {
+        return (Object.prototype.hasOwnProperty.call(d, 'accountable')? 1 : 0) +
+          (Object.prototype.hasOwnProperty.call(d, 'helpers') ? d.helpers.length : 0) + 1;
+      })
+      .sort(function (a, b) {
+        if (a && a.value && b && b.value) {
+          return b.value - a.value;
+        } else {
+          return 0;
+        }
+      });
 
     console.log('data after running hierarchy:');
     console.log(root);
 
     console.log('data after running circle packing:');
-    console.log(boo(root).descendants());
+    this.circles = boo(root).descendants();
+    console.log(this.circles);
   }
 }
