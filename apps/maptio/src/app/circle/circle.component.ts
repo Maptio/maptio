d5a4756acf0a25f4c88ce5405b40b81cabd6349a
Romek Goj <roman.goj@gmail.com>
2021-04-06 14:56:29 +0100

    Clean up scaling circles

--- apps/maptio/src/app/circle/circle.component.ts
+++ apps/maptio/src/app/circle/circle.component.ts
@@ -12,22 +12,15 @@ import { InitiativeNode } from '../shared/initiative.model';
 export class CircleComponent implements OnInit {
   @Input() circle!: InitiativeNode;
 
-  centerX = 500;
-  centerY = 500;
   defaultRadius = 500;
 
-  parentX = 0;
-  parentY = 0;
-  parentRadius = 0;
   x = 0;
   y = 0;
-  radius = 0;
-  diameter = 0;
   scale = 1;
+
   infoX = 0;
   infoY = 0;
   infoSize = 0;
-  transformOrigin = '0 0';
 
   // TODO: Move calculations into typescript code
   math = Math;
@@ -35,40 +28,15 @@ export class CircleComponent implements OnInit {
   constructor(private circleMapService: CircleMapService) {}
 
   ngOnInit() {
-    this.radius = this.circle.r;
-    this.diameter = this.radius * 2;
-    this.scale = this.diameter / 1000;
-
     if (this.circle.data.isPrimary) {
-      this.parentX = this.centerX;
-      this.parentY = this.centerY;
-      this.parentRadius = this.defaultRadius;
-
       this.x = this.circle.x / 10;
       this.y = this.circle.y / 10;
-
-      this.scale = this.radius / this.parentRadius;
-
-      this.transformOrigin = '0 0';
-    } else if (this.circle.parent && this.circle.parent.parent) {
-      // This achieves the correct size (322) if no translate applied
-      // and if a translate of (-100 and -100) is artificially applied for testing!
-      // So this scaling factor works!
-      this.scale = this.circle.r / this.circle.parent.r;
-      // const parentScale = this.circle.parent.r / this.circle.parent.parent.r;
-      const parentScale = this.circle.parent.r / 500;
-
-      this.transformOrigin = '0 0';
-
+      this.scale = this.circle.r / this.defaultRadius;
+    } else if (this.circle.parent) {
+      const parentScale = this.circle.parent.r / this.defaultRadius;
       this.x = (this.circle.x - this.circle.parent.x) / 10 / parentScale;
       this.y = (this.circle.y - this.circle.parent.y) / 10 / parentScale;
-
-      // this.parentX = this.circle.parent.x;
-      // this.parentY = this.circle.parent.y;
-      // this.x = this.circle.x - this.circle.parent.x;
-      // this.y = this.circle.y - this.circle.parent.y;
-      // this.scale = this.circle.r / this.defaultRadius;
-      // this.scale = 0.9;
+      this.scale = this.circle.r / this.circle.parent.r;
     } else {
       console.error('It should be impossible for a circle that is not a primary circle to not have a parent!');
     }
