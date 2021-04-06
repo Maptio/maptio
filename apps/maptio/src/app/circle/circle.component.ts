ed719cf4ab6d41485a23ce87bd395360211305b8
Romek Goj <roman.goj@gmail.com>
2021-04-06 10:40:16 +0100

    Move calculating circle position out of template

--- apps/maptio/src/app/circle/circle.component.ts
+++ apps/maptio/src/app/circle/circle.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 import { CircleMapService } from '../shared/circle-map.service';
 import { InitiativeNode } from '../shared/initiative.model';
@@ -9,13 +9,23 @@ import { InitiativeNode } from '../shared/initiative.model';
   templateUrl: './circle.component.html',
   styleUrls: ['./circle.component.scss'],
 })
-export class CircleComponent {
+export class CircleComponent implements OnInit {
   @Input() circle!: InitiativeNode;
+
+  x = 0;
+  y = 0;
+  diameter = 0;
 
   // TODO: Move calculations into typescript code
   math = Math;
 
   constructor(private circleMapService: CircleMapService) {}
+
+  ngOnInit() {
+    this.x = this.circle.x - this.circle.r / Math.sqrt(2)
+    this.y = this.circle.y - this.circle.r / Math.sqrt(2)
+    this.diameter = this.circle.r * 2 / Math.sqrt(2);
+  }
 
   onClick($event: MouseEvent) {
     console.log(`CircleComponent.onClick fired for: ${this.circle.data.name}`);
