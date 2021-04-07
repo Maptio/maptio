df6fe5f70404db19c0b8f7214c5a2aca7ec3fbdb
Romek Goj <roman.goj@gmail.com>
2021-04-07 15:18:49 +0100

    Show and style tags

--- apps/maptio/src/app/shared/initiative.model.ts
+++ apps/maptio/src/app/shared/initiative.model.ts
@@ -4,11 +4,19 @@ import { HierarchyCircularNode } from 'd3-hierarchy';
 export interface Initiative {
   name: string;
   color: string;
+
   accountable?: {
     picture: string;
+    name: string;
   };
   helpers?: {
     picture: string;
+    name: string;
+  }[];
+
+  tags?: {
+    name: string;
+    color: string;
   }[];
 
   // TODO: Consider moving these additions for state and type elsewhere
