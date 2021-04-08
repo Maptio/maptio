2dc18412a453b03d19f41de41270836c83fc5312
Romek Goj <roman.goj@gmail.com>
2021-04-08 11:24:37 +0100

    Add Angular Material

--- apps/maptio/src/app/app.module.ts
+++ apps/maptio/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -24,6 +25,7 @@ import { CircleInfoComponent } from './circle-info/circle-info.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpClientModule,
     AppRoutingModule,
   ],
