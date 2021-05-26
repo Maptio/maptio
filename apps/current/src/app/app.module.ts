ae5216852179b7e781e79d5f12065ac623b03fca
Romek Goj <roman.goj@gmail.com>
2021-05-26 15:45:07 +0100

    Move to using Angular CLI for the current app

--- apps/current/src/app/app.module.ts
+++ apps/current/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { Location, LocationStrategy, PathLocationStrategy, APP_BASE_HREF } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-// import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { RouterModule } from "@angular/router";
 import { AppComponent } from "./app.component";
 import { CoreModule } from "./core/core.module";
@@ -9,7 +8,7 @@ import { AppRoutingModule } from "./app.routing";
 import { AnalyticsModule } from "./core/analytics.module";
 import { SharedModule } from "./shared/shared.module";
 import { MarkdownModule, MarkedOptions, MarkedRenderer } from "ngx-markdown";
-import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+
 
 export function markedOptionsFactory(): MarkedOptions {
     const renderer = new MarkedRenderer();
@@ -69,32 +68,4 @@ export function markedOptionsFactory(): MarkedOptions {
     entryComponents: [AppComponent],
     bootstrap: [AppComponent]
 })
-
-export class AppModule {
-
-}
-
-if ((module as any).hot) {
-    (module as any).hot.accept();
-    console.clear();
-    console.log('[HMR] Accepting module hot update.');
-    const applicationTagName = 'maptio-app';
-    tryRemoveApplicationNode(applicationTagName);
-    tryBootstrapNewApplication(applicationTagName);
-}
-
-function tryRemoveApplicationNode(tagName:string) {
-    const currentApplicationNode = document.getElementsByTagName(tagName)[0];
-    if (currentApplicationNode) {
-        const parent = currentApplicationNode.parentNode;
-        parent.removeChild(currentApplicationNode);
-    }
-}
-
-function tryBootstrapNewApplication(tagName:string) {
-    const newNode = document.createElement(tagName);
-    document.getElementsByTagName('body')[0].insertAdjacentElement('beforeend', newNode);
-
-    const newAppModule = require('./app.module').AppModule;
-    platformBrowserDynamic().bootstrapModule(newAppModule);
-}
+export class AppModule { }
