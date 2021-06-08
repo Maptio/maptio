6c8d7d0349f91b612365d0ca318509010c761443
Romek Goj <roman.goj@gmail.com>
2021-06-08 13:38:28 +0100

    Add angular localize - needed by ng-bootstrap

--- apps/current/src/polyfills.ts
+++ apps/current/src/polyfills.ts
@@ -1,3 +1,7 @@
+/***************************************************************************************************
+ * Load `$localize` onto the global scope - used if i18n tags appear in Angular templates.
+ */
+import '@angular/localize/init';
 import "core-js/es6";
 import "core-js/es7/reflect";
 import "zone.js/dist/zone";
