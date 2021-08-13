2587d47d8a7116193f633a1412941f89683b822f
Romek Goj <roman.goj@gmail.com>
2021-08-13 21:05:50 +0100

    Add all buttons to size input

--- apps/maptio/src/app/modules/workspace/components/data-entry/details/parts/size/input-size.component.ts
+++ apps/maptio/src/app/modules/workspace/components/data-entry/details/parts/size/input-size.component.ts
@@ -1,37 +1,59 @@
-import { Component, OnInit, Output, Input, EventEmitter, ChangeDetectorRef, ViewChild, ElementRef } from '@angular/core';
-import { Permissions } from '../../../../../../../shared/model/permission.data';
+import {
+  Component,
+  Output,
+  Input,
+  EventEmitter,
+  ChangeDetectorRef,
+  OnChanges,
+  SimpleChanges
+} from '@angular/core';
+
+import { Permissions } from '@maptio-shared/model/permission.data';
+
 
 @Component({
   selector: 'initiative-input-size',
   templateUrl: './input-size.component.html',
   styleUrls: ['./input-size.component.scss']
 })
-export class InitiativeInputSizeComponent {
+export class InitiativeInputSizeComponent implements OnChanges {
   @Input() sizeAdjustment: string;
-  @Input() isEditMode: boolean;
   @Input() isUnauthorized: boolean;
 
   @Output() save: EventEmitter<number> = new EventEmitter<number>();
-
-  @ViewChild("inputName") inputName: ElementRef;
 
   size = 0;
 
   constructor(private cd: ChangeDetectorRef) { }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.sizeAdjustment && changes.sizeAdjustment.currentValue) {
+      this.size = Number.parseInt(changes.sizeAdjustment.currentValue);
+      this.cd.markForCheck();
+    }
+  }
+
   saveSize(newSize: number) {
+    console.log(typeof newSize);
     this.save.emit(newSize);
     this.size = newSize;
     this.cd.markForCheck();
   }
 
-  onChange(newSize: number) {
-    console.log('got new size', newSize);
-    this.saveSize(newSize);
+  onEdit(newSize: string) {
+    console.log(typeof newSize);
+    this.saveSize(Number.parseInt(newSize));
   }
 
-  onClick() {
-    this.isEditMode = true;
-    this.cd.markForCheck();
+  onIncrease() {
+    this.saveSize(this.size + 1);
+  }
+
+  onDecrease() {
+    this.saveSize(this.size - 1);
+  }
+
+  onReset() {
+    this.saveSize(0);
   }
 }
