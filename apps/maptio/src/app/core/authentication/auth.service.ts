abdc50b45a59004288a433f46cdfd4c40bc64df3
Romek Goj <roman.goj@gmail.com>
2021-09-22 14:51:22 +0100

    Clean up more user-related code at least a bit

--- apps/maptio/src/app/core/authentication/auth.service.ts
+++ apps/maptio/src/app/core/authentication/auth.service.ts
@@ -1,44 +1,43 @@
-import { Injectable, isDevMode } from "@angular/core";
-import { HttpClient, HttpHeaders } from "@angular/common/http";
-import { Router } from "@angular/router";
+import { Injectable, isDevMode } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 import { map } from 'rxjs/operators';
 
 import { JwtHelperService } from '@auth0/angular-jwt';
 
-import { TeamFactory } from "./../http/team/team.factory";
+import { TeamFactory } from './../http/team/team.factory';
 import {
   UserRoleService,
-  Permissions
-} from "../../shared/model/permission.data";
-import { Angulartics2Mixpanel } from "angulartics2/mixpanel";
-import { environment } from "../../config/environment";
-import { LoaderService } from "../../shared/components/loading/loader.service";
-import { Observable ,  Subject } from "rxjs";
-import { AuthConfiguration } from "./auth.config";
-import { DatasetFactory } from "../http/map/dataset.factory";
-import { UserFactory } from "../http/user/user.factory";
-import { User } from "../../shared/model/user.data";
-import { EmitterService } from "../services/emitter.service";
-import { uniq } from "lodash-es";
-import * as LogRocket from "logrocket";
-import { Intercom } from "ng-intercom";
+  Permissions,
+} from '../../shared/model/permission.data';
+import { Angulartics2Mixpanel } from 'angulartics2/mixpanel';
+import { environment } from '../../config/environment';
+import { LoaderService } from '../../shared/components/loading/loader.service';
+import { Observable, Subject } from 'rxjs';
+import { AuthConfiguration } from './auth.config';
+import { DatasetFactory } from '../http/map/dataset.factory';
+import { UserFactory } from '../http/user/user.factory';
+import { User } from '../../shared/model/user.data';
+import { EmitterService } from '../services/emitter.service';
+import { uniq } from 'lodash-es';
+import * as LogRocket from 'logrocket';
 
 @Injectable()
 export class Auth {
   private MAPTIO_INTERNAL_EMAILS = [
-    "safiyya.babio@gmail.com",
-    "hello@tomnixon.co.uk",
-    "karlparton@gmail.com",
-    "lisa@reimaginaire.com",
-    "hellochandnipatel@gmail.com",
-    "clemens.anzmann@googlemail.com",
-    "maptio@robski.net"
+    'safiyya.babio@gmail.com',
+    'hello@tomnixon.co.uk',
+    'karlparton@gmail.com',
+    'lisa@reimaginaire.com',
+    'hellochandnipatel@gmail.com',
+    'clemens.anzmann@googlemail.com',
+    'maptio@robski.net',
   ];
 
   private user$: Subject<User> = new Subject();
   private permissions: Permissions[] = [];
-  private fullstory: any = window["FS"];
+  private fullstory: any = window['FS']; // eslint-disable-line @typescript-eslint/no-explicit-any
 
   constructor(
     private http: HttpClient,
@@ -51,14 +50,13 @@ export class Auth {
     private loader: LoaderService,
     private userRoleService: UserRoleService,
     private analytics: Angulartics2Mixpanel,
-    private intercom: Intercom
   ) {}
 
   public logout(): void {
     this.clear();
     this.user$.unsubscribe();
-    this.router.navigateByUrl("/logout");
-    this.analytics.eventTrack("Logout", {});
+    this.router.navigateByUrl('/logout');
+    this.analytics.eventTrack('Logout', {});
     this.fullstory.shutdown();
   }
 
@@ -68,15 +66,15 @@ export class Auth {
    * This should be used in favour of localStorage.clear() whenever local storage needs to be cleaned out
    */
   public clear() {
-    let persist = new Map<string, string>();
+    const persist = new Map<string, string>();
     for (let i = 0; i < localStorage.length; i++) {
-      if (localStorage.key(i).indexOf("map_settings") > -1) {
+      if (localStorage.key(i).indexOf('map_settings') > -1) {
         persist.set(
           localStorage.key(i),
           localStorage.getItem(localStorage.key(i))
         );
       }
-      if (localStorage.key(i).indexOf("redirectUrl") > -1) {
+      if (localStorage.key(i).indexOf('redirectUrl') > -1) {
         persist.set(
           localStorage.key(i),
           localStorage.getItem(localStorage.key(i))
@@ -106,21 +104,21 @@ export class Auth {
    * Checks if Auth0 Management API is still valid
    */
   public authenticationProviderAuthenticated() {
-    return this.tokenNotExpired("access_token");
+    return this.tokenNotExpired('access_token');
   }
 
   /**
    * Checks if Maptio API is still valid
    */
   public internalApiAuthenticated() {
-    return this.tokenNotExpired("maptio_api_token");
+    return this.tokenNotExpired('maptio_api_token');
   }
 
   /**
    * Checks is ID token is still valid
    */
   public authenticated(): boolean {
-    return this.tokenNotExpired("id_token");
+    return this.tokenNotExpired('id_token');
   }
 
   // TODO: This gets called hundreds of times every second and it's not an entirely trivial
@@ -141,12 +139,13 @@ export class Auth {
         realm: environment.CONNECTION_NAME,
         username: email,
         password: password,
-        scope: "openid profile api invite",
-        audience: environment.MAPTIO_API_URL
+        scope: 'openid profile api invite',
+        audience: environment.MAPTIO_API_URL,
       },
-      function(err: any, authResult: any) {
+
+      function (err: any, authResult: any) { // eslint-disable-line @typescript-eslint/no-explicit-any
         if (authResult.accessToken) {
-          EmitterService.get("maptio_api_token").emit(authResult.accessToken);
+          EmitterService.get('maptio_api_token').emit(authResult.accessToken);
         }
       }
     );
@@ -156,36 +155,41 @@ export class Auth {
     return new Promise((resolve, reject) => {
       this.configuration.getWebAuth().checkSession(
         {
-          scope: "openid profile api invite",
+          scope: 'openid profile api invite',
           audience: environment.MAPTIO_API_URL,
-          responseType: "token id_token",
+          responseType: 'token id_token',
           redirectUri: `${window.location.protocol}//${
             window.location.hostname
           }${
-            window.location.port === "" ? "" : `:${window.location.port}`
+            window.location.port === '' ? '' : `:${window.location.port}`
           }/authorize`,
-          connection: "google-oauth2"
+          connection: 'google-oauth2',
         },
-        function(err: any, authResult: any) {
+        function (err: any, authResult: any) {
           if (err) {
             console.error(err);
             reject(err);
           }
           resolve({
             accessToken: authResult.accessToken,
-            idToken: authResult.idToken
+            idToken: authResult.idToken,
           });
         }
       );
     });
   }
 
+  /**
+   * Retrieve all user information stored in Auth0
+   * @param userId
+   * @returns A user object created from the data stored in Auth0
+   */
   public getUserInfo(userId: string): Promise<User> {
     return this.configuration.getAccessToken().then((token: string) => {
       const httpOptions = {
         headers: new HttpHeaders({
-          Authorization: 'Bearer ' + token
-        })
+          Authorization: 'Bearer ' + token,
+        }),
       };
 
       return this.http
@@ -196,7 +200,7 @@ export class Auth {
         .pipe(
           map((input: any) => {
             return User.create().deserialize(input);
-          }),
+          })
         )
         .toPromise();
     });
@@ -207,15 +211,15 @@ export class Auth {
   }
 
   public getUser(): Observable<User> {
-    let profileString = localStorage.getItem("profile");
+    let profileString = localStorage.getItem('profile');
 
     if (profileString) {
       Promise.all([
         this.getUserInfo(JSON.parse(profileString).user_id),
-        this.userFactory.get(JSON.parse(profileString).user_id)
+        this.userFactory.get(JSON.parse(profileString).user_id),
       ])
         .then(([auth0User, databaseUser]: [User, User]) => {
-          let user = auth0User;
+          const user = auth0User;
           user.teams = uniq(databaseUser.teams); // HACK : where does the duplication comes from?
           user.shortid = databaseUser.shortid;
           return user;
@@ -224,24 +228,25 @@ export class Auth {
           return user.teams.length > 0
             ? this.teamFactory
                 .get(user.teams)
-                .then(teams =>
-                  teams.filter(team => team.isExample).map(team => team.team_id)
+                .then((teams) =>
+                  teams
+                    .filter((team) => team.isExample)
+                    .map((team) => team.team_id)
                 )
-                .then(exampleTeamIds => {
+                .then((exampleTeamIds) => {
                   user.exampleTeamIds = exampleTeamIds;
                   return user;
                 })
             : user;
         })
         .then((user: User) => {
-          console.log("user", user);
           this.permissions = this.userRoleService.get(user.userRole);
           return user;
         })
         .then((user: User) => {
-          this.datasetFactory.get(user).then(ds => {
+          this.datasetFactory.get(user).then((ds) => {
             user.datasets = uniq(ds);
-            EmitterService.get("headerUser").emit(user);
+            EmitterService.get('headerUser').emit(user);
             this.user$.next(user);
           });
         });
@@ -252,10 +257,10 @@ export class Auth {
   }
 
   public setUser(profile: any): Promise<boolean> {
-    localStorage.setItem("profile", JSON.stringify(profile));
+    localStorage.setItem('profile', JSON.stringify(profile));
     return this.userFactory
       .get(profile.user_id)
-      .then(user => {
+      .then((user) => {
         this.user$.next(user);
         return Promise.resolve<boolean>(true);
       })
@@ -279,17 +284,17 @@ export class Auth {
   // }
 
   public googleSignIn() {
-    this.signin("google-oauth2");
+    this.signin('google-oauth2');
   }
 
   private signin(connection: string) {
     this.configuration.getWebAuth().authorize({
-      scope: "profile openid email",
-      responseType: "token",
+      scope: 'profile openid email',
+      responseType: 'token',
       redirectUri: `${window.location.protocol}//${window.location.hostname}${
-        window.location.port === "" ? "" : `:${window.location.port}`
+        window.location.port === '' ? '' : `:${window.location.port}`
       }/authorize`,
-      connection: connection
+      connection: connection,
     });
   }
 
@@ -301,30 +306,30 @@ export class Auth {
           realm: environment.CONNECTION_NAME,
           username: email,
           password: password,
-          scope: "profile openid email"
+          scope: 'profile openid email',
         },
-        function(err: any, authResult: any) {
+        function (err: any, authResult: any) {
           if (err) {
-            EmitterService.get("loginErrorMessage").emit(err.description);
+            EmitterService.get('loginErrorMessage').emit(err.description);
             return;
           }
           this.user$ = new Subject();
-          localStorage.setItem("id_token", authResult.idToken);
+          localStorage.setItem('id_token', authResult.idToken);
 
           if (authResult.accessToken) {
             this.configuration.getWebAuth().client.userInfo(
               authResult.accessToken,
-              function(err: Error, profile: any) {
+              function (err: Error, profile: any) {
                 profile.user_id = profile.sub;
                 profile.sub = undefined;
                 this.loader.show();
                 this.loginMaptioApi(email, password);
 
-                EmitterService.get("maptio_api_token").subscribe(
+                EmitterService.get('maptio_api_token').subscribe(
                   (token: string) => {
-                    if (localStorage.getItem("maptio_api_token"))
-                      localStorage.removeItem("maptio_api_token");
-                    localStorage.setItem("maptio_api_token", token);
+                    if (localStorage.getItem('maptio_api_token'))
+                      localStorage.removeItem('maptio_api_token');
+                    localStorage.setItem('maptio_api_token', token);
 
                     return this.setUser(profile).then((isSuccess: boolean) => {
                       if (isSuccess) {
@@ -349,31 +354,31 @@ export class Auth {
                                 user_id: user.user_id,
                                 email: user.email,
                                 hostname: window.location.hostname,
-                                isInternal: isMaptioTeam
+                                isInternal: isMaptioTeam,
                               });
-                              this.analytics.eventTrack("Login", {
+                              this.analytics.eventTrack('Login', {
                                 email: user.email,
                                 firstname: user.firstname,
-                                lastname: user.lastname
+                                lastname: user.lastname,
                               });
                               LogRocket.identify(user.user_id, {
                                 name: user.name,
-                                email: user.email
+                                email: user.email,
                               });
                               this.fullstory.identify(user.user_id, {
                                 displayName: user.name,
-                                email: user.email
+                                email: user.email,
                               });
                               this.intercom.update({
                                 app_id: environment.INTERCOM_APP_ID,
                                 email: user.email,
                                 name: user.name,
                                 avatar: {
-                                  type: "avatar",
-                                  image_url: user.picture
+                                  type: 'avatar',
+                                  image_url: user.picture,
                                 },
                                 is_invited: user.isInvitationSent,
-                                user_id: user.user_id
+                                user_id: user.user_id,
                               });
 
                               return user;
@@ -386,21 +391,22 @@ export class Auth {
                             // let welcomeURL = user.datasets.length === 1 ? `/map/${user.datasets[0]}/welcome/initiatives` : `/home`;
                             this.loader.hide();
                             let redirectUrl =
-                              localStorage.getItem("redirectUrl") &&
-                              localStorage.getItem("redirectUrl") !== null &&
-                              localStorage.getItem("redirectUrl") !== "null"
-                                ? localStorage.getItem("redirectUrl")
+                              localStorage.getItem('redirectUrl') &&
+                              localStorage.getItem('redirectUrl') !== null &&
+                              localStorage.getItem('redirectUrl') !== 'null'
+                                ? localStorage.getItem('redirectUrl')
                                 : null;
                             this.router.navigateByUrl(
-                              redirectUrl ? redirectUrl : "/home"
+                              redirectUrl ? redirectUrl : '/home'
                             );
 
                             // Prevent the URL from being persisted between sessions
-                            if (redirectUrl) localStorage.removeItem("redirectUrl");
+                            if (redirectUrl)
+                              localStorage.removeItem('redirectUrl');
                           });
                       } else {
                         this.errorService.handleError(
-                          "Something has gone wrong ! Try again ?"
+                          'Something has gone wrong ! Try again ?'
                         );
                       }
                     });
