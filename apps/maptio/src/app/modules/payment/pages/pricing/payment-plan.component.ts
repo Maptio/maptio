e22a5c5642e5978fb6b49ccac0b7b14d89b82190
Romek Goj <roman.goj@gmail.com>
2021-09-30 13:46:19 +0100

    Refactor the code for the pricing page

---
+++ apps/maptio/src/app/modules/payment/pages/pricing/payment-plan.component.ts
@@ -0,0 +1,19 @@
+import { Component, HostBinding, Input } from '@angular/core';
+
+import { Auth } from '@maptio-core/authentication/auth.service';
+
+
+@Component({
+  selector: 'maptio-payment-plan',
+  templateUrl: './payment-plan.component.html',
+})
+export class PaymentPlanComponent {
+  @HostBinding('class') classes = 'col-12 col-md-3 accent-blue rounded-bottom box-shadow mx-2 my-3';
+
+  @Input() name: string;
+  @Input() price: number;
+  @Input() text: number;
+  @Input() billingLink: string;
+
+  constructor(public auth: Auth) { }
+}
--- apps/maptio/src/app/modules/payment/payment.module.ts
+++ apps/maptio/src/app/modules/payment/payment.module.ts
@@ -1,19 +1,25 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { SanitizerModule } from "../../shared/sanitizer.module";
-import { CheckoutComponent } from "./pages/checkout/checkout.page";
-import { PricingComponent } from "./pages/pricing/pricing.page";
-import { PaymentRoutingModule } from "./payment.routing";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SanitizerModule } from '@maptio-shared/sanitizer.module';
+
+import { CheckoutComponent } from './pages/checkout/checkout.page';
+import { PricingComponent } from './pages/pricing/pricing.page';
+import { PaymentPlanComponent } from './pages/pricing/payment-plan.component';
+import { PaymentRoutingModule } from './payment.routing';
 
 
 @NgModule({
-    declarations: [
-        CheckoutComponent,
-        PricingComponent
-    ],
-    imports: [CommonModule,
-        SanitizerModule,
-        PaymentRoutingModule],
-    providers: [],
+  declarations: [
+    CheckoutComponent,
+    PricingComponent,
+    PaymentPlanComponent
+  ],
+  imports: [
+    CommonModule,
+    SanitizerModule,
+    PaymentRoutingModule
+  ],
+  providers: [],
 })
-export class PaymentModule { }
+export class PaymentModule {}
