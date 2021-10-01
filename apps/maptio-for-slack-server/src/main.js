fd06d9a56ed8bf0c247774567f4b78e9f056ebd9
Romek Goj <roman.goj@gmail.com>
2021-10-01 09:25:32 +0100

    Copy in example Slack Bolt app code

--- apps/maptio-for-slack-server/src/main.js
+++ apps/maptio-for-slack-server/src/main.js
@@ -1,18 +1,69 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
+//
+// Based on code from https://github.com/slackapi/bolt-js-getting-started-app
+//
 
-import * as express from 'express';
+const { App } = require('@slack/bolt');
 
-const app = express();
-
-app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to maptio-for-slack-server!' });
+// Initializes your app with your bot token and app token
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  socketMode: true,
+  appToken: process.env.SLACK_APP_TOKEN
 });
 
-const port = process.env.port || 3333;
-const server = app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/api`);
+// Listens to incoming messages that contain "hello"
+app.message('hello', async ({ message, say }) => {
+  // say() sends a message to the channel where the event was triggered
+  await say({
+    blocks: [
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": `Hey there <@${message.user}>!`
+        },
+        "accessory": {
+          "type": "button",
+          "text": {
+            "type": "plain_text",
+            "text": "Click Me"
+          },
+          "action_id": "button_click"
+        }
+      }
+    ],
+    text: `Hey there <@${message.user}>!`
+  });
 });
-server.on('error', console.error);
+
+app.action('button_click', async ({ body, ack, say }) => {
+  // Acknowledge the action
+  await ack();
+  await say(`<@${body.user.id}> clicked the button`);
+});
+
+(async () => {
+  // Start your app
+  await app.start(process.env.PORT || 3000);
+
+  console.log('⚡️ Bolt app is running!');
+})();
+
+
+//
+// Code scaffolded with Nx
+//
+
+// import * as express from 'express';
+
+// const app = express();
+
+// app.get('/api', (req, res) => {
+//   res.send({ message: 'Welcome to maptio-for-slack-server!' });
+// });
+
+// const port = process.env.port || 3333;
+// const server = app.listen(port, () => {
+//   console.log(`Listening at http://localhost:${port}/api`);
+// });
+// server.on('error', console.error);
