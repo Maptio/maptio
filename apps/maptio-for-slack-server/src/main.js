2cdfa6d6c2448a6040eb538e92393c66a2e82905
Romek Goj <roman.goj@gmail.com>
2021-10-05 09:18:14 +0100

    Remove scaffolded and example code

--- apps/maptio-for-slack-server/src/main.js
+++ apps/maptio-for-slack-server/src/main.js
@@ -28,63 +28,6 @@ const app = new App({
 
 
 (async () => {
-  // Start your app
   await app.start(process.env.PORT || 3001);
-
   console.log('⚡️ Bolt app is running!');
 })();
-
-
-//
-// Code from most basic Slack/Bolt tutorials/examples
-//
-
-// // Listens to incoming messages that contain "hello"
-// app.message('hello', async ({ message, say }) => {
-//   // say() sends a message to the channel where the event was triggered
-//   await say({
-//     blocks: [
-//       {
-//         "type": "section",
-//         "text": {
-//           "type": "mrkdwn",
-//           "text": `Hey there <@${message.user}>!`
-//         },
-//         "accessory": {
-//           "type": "button",
-//           "text": {
-//             "type": "plain_text",
-//             "text": "Click Me"
-//           },
-//           "action_id": "button_click"
-//         }
-//       }
-//     ],
-//     text: `Hey there <@${message.user}>!`
-//   });
-// });
-
-// app.action('button_click', async ({ body, ack, say }) => {
-//   // Acknowledge the action
-//   await ack();
-//   await say(`<@${body.user.id}> clicked the button`);
-// });
-
-
-//
-// Code scaffolded with Nx
-//
-
-// import * as express from 'express';
-
-// const app = express();
-
-// app.get('/api', (req, res) => {
-//   res.send({ message: 'Welcome to maptio-for-slack-server!' });
-// });
-
-// const port = process.env.port || 3333;
-// const server = app.listen(port, () => {
-//   console.log(`Listening at http://localhost:${port}/api`);
-// });
-// server.on('error', console.error);
