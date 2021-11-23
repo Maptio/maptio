cd318a12e3afea344f48ad090987a42454ca85ce
Romek Goj <roman.goj@gmail.com>
2021-11-23 21:09:00 +0000

    Reformat and clean up login module

--- apps/maptio/src/app/modules/login/login.module.ts
+++ apps/maptio/src/app/modules/login/login.module.ts
@@ -1,32 +1,36 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { SanitizerModule } from '@maptio-shared/sanitizer.module';
+import { ImageModule } from '@maptio-shared/image.module';
+import { GoogleSignInComponent } from '@maptio-shared/components/buttons/google-signin.component';
+
+import { LoginRoutingModule } from './login.routing';
 import { LoginComponent } from './pages/login/login.page';
 import { LogoutComponent } from './pages/logout/logout.page';
-import { AuthorizeComponent } from './pages/authorize/authorize.page';
-import { ChangePasswordComponent } from './pages/forgot-password/change-password.page';
 import { SignupComponent } from './pages/sign-up/signup.page';
-import { ReactiveFormsModule } from '@angular/forms';
-import { SanitizerModule } from '../../shared/sanitizer.module';
-import { GoogleSignInComponent } from '../../shared/components/buttons/google-signin.component';
-import { LoginRoutingModule } from './login.routing';
+import { AuthorizeComponent } from './pages/authorize/authorize.page';
 import { ProfilePage } from './pages/profile/profile.page';
-import { ImageModule } from '../../shared/image.module';
-
+import { ChangePasswordComponent } from './pages/forgot-password/change-password.page';
 
 @NgModule({
-    declarations: [
-        LoginComponent,
-        LogoutComponent,
-        AuthorizeComponent,
-        ChangePasswordComponent,
-        SignupComponent,
-        GoogleSignInComponent,
-        ProfilePage],
-    imports: [CommonModule,
-        ReactiveFormsModule,
-        SanitizerModule,
-        LoginRoutingModule,
-        ImageModule],
-    providers: [],
+  declarations: [
+    GoogleSignInComponent,
+    LoginComponent,
+    LogoutComponent,
+    SignupComponent,
+    AuthorizeComponent,
+    ProfilePage,
+    ChangePasswordComponent,
+  ],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    SanitizerModule,
+    ImageModule,
+    LoginRoutingModule,
+  ],
+  providers: [],
 })
-export class LoginModule { }
+export class LoginModule {}
