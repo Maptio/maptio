c9609c8a2a5a5b92bfc27aba53f3f5b4f888873e
Romek Goj <roman.goj@gmail.com>
2021-12-05 15:31:05 +0000

    Add auth config to admin app

--- apps/maptio-admin/src/app/app.module.ts
+++ apps/maptio-admin/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+
+import { AuthModule } from '@auth0/auth0-angular';
+
+import { environment } from '../environments/environment';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+    AuthModule.forRoot({ ...environment.auth }),
   ],
   providers: [],
   bootstrap: [AppComponent],
