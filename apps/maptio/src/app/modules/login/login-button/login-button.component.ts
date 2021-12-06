9f8d421ec28922296c7ba35c4f667115d55b97a2
Romek Goj <roman.goj@gmail.com>
2021-12-06 14:04:00 +0000

    Make login button work

--- apps/maptio/src/app/modules/login/login-button/login-button.component.ts
+++ apps/maptio/src/app/modules/login/login-button/login-button.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { AuthService } from '@auth0/auth0-angular';
+
 
 @Component({
   selector: 'maptio-login-button',
   templateUrl: './login-button.component.html',
   styleUrls: ['./login-button.component.scss']
 })
-export class LoginButtonComponent implements OnInit {
+export class LoginButtonComponent {
+  constructor(public auth: AuthService) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  loginWithRedirect() {
+    this.auth.loginWithRedirect();
   }
 
+  signupWithRedirect() {
+    this.auth.loginWithRedirect({
+      screen_hint: 'signup'
+    });
+  }
 }
