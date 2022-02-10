f5fac645cc3432f46fb5cfdbed97d7a696a6017e
Romek Goj <roman.goj@gmail.com>
2022-02-10 11:31:43 +0000

    Scaffold new error page component

---
+++ apps/maptio/src/app/core/error/error.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'maptio-error',
+  templateUrl: './error.page.html',
+  styleUrls: ['./error.page.scss']
+})
+export class ErrorPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
