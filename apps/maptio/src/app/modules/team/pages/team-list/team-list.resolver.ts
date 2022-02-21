69522d556b596bde43bf9d1ebfb32fdfdac42af6
Romek Goj <roman.goj@gmail.com>
2022-02-21 15:56:06 +0000

    Simply team list resolvert

--- apps/maptio/src/app/modules/team/pages/team-list/team-list.resolver.ts
+++ apps/maptio/src/app/modules/team/pages/team-list/team-list.resolver.ts
@@ -2,59 +2,27 @@ import { Injectable } from '@angular/core';
 import { Resolve } from '@angular/router';
 
 import { Observable } from 'rxjs';
-import { map, mergeMap, first } from 'rxjs/operators';
+import { map, first } from 'rxjs/operators';
 
-import { differenceBy, sortBy, isEmpty } from 'lodash-es';
-
-import { Auth } from '@maptio-core/authentication/auth.service';
-import { TeamFactory } from '@maptio-core/http/team/team.factory';
 import { Team } from '@maptio-shared/model/team.data';
-import { User } from '@maptio-shared/model/user.data';
 import { UserService } from '@maptio-shared/services/user/user.service';
 
 
 @Injectable()
 export class TeamListComponentResolver implements Resolve<Team[]> {
-  constructor(
-    private teamFactory: TeamFactory,
-    private auth: Auth,
-    private userService: UserService
-  ) {}
+  constructor(private userService: UserService) {}
 
   resolve(): Observable<Team[]> {
-    return this.auth.getUser().pipe(
+    // TODO: This could be reactive, there's no need for this resolver,
+    // really, but relying on the old code architecture to avoid propagating
+    // all the reactive code changes throughout the codebase just yet - though
+    // that is the eventual goal, just not in the current piece of work (auth
+    // refactoring) where I'm making this change.
+    return this.userService.userWithTeamsAndDatasets$.pipe(
       first(),
-      mergeMap((user) => {
-        return isEmpty(user.teams)
-          ? Promise.resolve([])
-          : this.teamFactory.get(user.teams);
+      map(userWithTeamsAndDatasets => {
+        return userWithTeamsAndDatasets.teams;
       }),
-      map((teams: Team[]) => {
-        teams.forEach((team) => {
-          if (team) {
-            this.userService
-              .getUsersInfo(team.members)
-              .then((actualMembers: User[]) => {
-                const allDeleted = differenceBy(
-                  team.members,
-                  actualMembers,
-                  (member) => member.user_id
-                ).map((member) => {
-                  member.isDeleted = true;
-                  return member;
-                });
-                return actualMembers.concat(allDeleted);
-              })
-              .then((members) => (team.members = sortBy(members, (member) => member.name)));
-          }
-        });
-        return teams.filter((team) => {
-          return team !== undefined;
-        });
-      }),
-      map((teams) => {
-        return sortBy(teams, (team) => team.name);
-      })
     );
   }
 }
