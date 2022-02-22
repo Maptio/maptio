a49e2add69197684eda576d7a9388f8fd94893e1
Romek Goj <roman.goj@gmail.com>
2022-02-22 17:23:15 +0000

    Scaffold new component to house image upload code

---
+++ apps/maptio/src/app/shared/components/image-upload/image-upload.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'maptio-image-upload',
+  templateUrl: './image-upload.component.html',
+  styleUrls: ['./image-upload.component.scss']
+})
+export class ImageUploadComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
