7c6e537746b7ab5531df62afb39256aeec055324
Romek Goj <roman.goj@gmail.com>
2022-03-07 12:17:44 +0000

    Remove Auth0 Management API secret from code, ouch

--- apps/maptio/src/app/config/environment.ts
+++ apps/maptio/src/app/config/environment.ts
@@ -1,6 +1,4 @@
 export const environment = {
-    AUTH0_MANAGEMENTAPI_KEY: "mjQumlN564UkegYxzZGLNhM0umeEsmdC",
-    AUTH0_MANAGEMENTAPI_SECRET: "YHMsevargwqFXmBt7I0rAjjkhCz_yQ6gb8-g4YLwQRvKI_B2at22r0MUmyENEXZ_",
     AUTH0_APP_KEY: "CRvF82hID2lNIMK4ei2wDz20LH7S5BMy",
     AUTH0_DOMAIN: "login.maptio.com",
     TOS_URL: "https://termsfeed.com/terms-conditions/f0e548940bde8842b1fb58637ae048c0",
