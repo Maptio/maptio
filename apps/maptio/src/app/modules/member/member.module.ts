1858e31c90e0f9a5f49913bc832b5e8d872503a7
Romek Goj <roman.goj@gmail.com>
2022-04-19 11:43:24 +0100

    Scaffold a new component for member info

---
+++ apps/maptio/src/app/modules/member/member.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MemberComponent } from './member/member.component';
+
+
+
+@NgModule({
+  declarations: [
+    MemberComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class MemberModule { }
