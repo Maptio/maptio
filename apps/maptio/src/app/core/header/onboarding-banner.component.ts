d5905536c70641656519daefea1abd6566842a6a
Romek Goj <roman.goj@gmail.com>
2022-05-09 14:28:04 +0100

    Show free trial time left

--- apps/maptio/src/app/core/header/onboarding-banner.component.ts
+++ apps/maptio/src/app/core/header/onboarding-banner.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { Team } from '@maptio-shared/model/team.data';
+
 
 @Component({
   selector: 'maptio-onboarding-banner',
   templateUrl: './onboarding-banner.component.html',
   styleUrls: ['./onboarding-banner.component.scss']
 })
-export class OnboardingBannerComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class OnboardingBannerComponent {
+  @Input() team?: Team;
 }
