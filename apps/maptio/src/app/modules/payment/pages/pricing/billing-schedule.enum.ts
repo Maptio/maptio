f674e93a7ed6134ff0e46a90c9f794ff3b63e866
Romek Goj <roman.goj@gmail.com>
2022-05-11 16:49:20 +0100

    Add radio group for annual/monthly billing toggle

---
+++ apps/maptio/src/app/modules/payment/pages/pricing/billing-schedule.enum.ts
@@ -0,0 +1,4 @@
+export enum BillingSchedule {
+  Monthly,
+  Annual,
+}
--- apps/maptio/src/app/modules/payment/pages/pricing/pricing.page.ts
+++ apps/maptio/src/app/modules/payment/pages/pricing/pricing.page.ts
@@ -3,6 +3,8 @@ import { Component } from '@angular/core';
 import { environment } from '@maptio-config/environment';
 import { UserService } from '@maptio-shared/services/user/user.service';
 
+import { BillingSchedule } from './billing-schedule.enum';
+
 
 @Component({
   selector: 'maptio-pricing',
@@ -13,6 +15,9 @@ export class PricingComponent {
   public BILLING_SMALL_PLAN = environment.BILLING_SMALL_PLAN;
   public BILLING_STANDARD_PLAN = environment.BILLING_STANDARD_PLAN;
   public BILLING_PORTAL = environment.BILLING_PORTAL;
+
+  BillingSchedule = BillingSchedule;
+  billingScheduleChoice = BillingSchedule.Monthly;
 
   constructor(public userService: UserService) { }
 }
