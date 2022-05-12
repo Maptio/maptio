66e82d586480fcbfba7a5aa044509843e7da6b01
Romek Goj <roman.goj@gmail.com>
2022-05-12 10:39:45 +0100

    Add annual options to all plans

--- apps/maptio/src/environments/environment.common.ts
+++ apps/maptio/src/environments/environment.common.ts
@@ -8,11 +8,18 @@ export const commonEnvironment = {
 
   BILLING_PLANS: {
     STARTER_MONTHLY_COST: '20',
-    STARTER_MONTHLY_URL: "https://maptio.chargebee.com/hosted_pages/plans/standard-plan12",
+    STARTER_MONTHLY_URL: 'https://maptio.chargebee.com/hosted_pages/plans/standard-plan12',
     STARTER_ANNUAL_COST: '18',
-    STARTER_ANNUAL_URL: "https://maptio.chargebee.com/hosted_pages/plans/annual_12_18",
+    STARTER_ANNUAL_URL: 'https://maptio.chargebee.com/hosted_pages/plans/annual_12_18',
 
-    SMALL_PLAN: "https://maptio.chargebee.com/hosted_pages/plans/standard-plan50",
-    STANDARD_PLAN: "https://maptio.chargebee.com/hosted_pages/plans/standard-plan",
+    SMALL_MONTHLY_COST: '50',
+    SMALL_MONTHLY_URL: 'https://maptio.chargebee.com/hosted_pages/plans/standard-plan50',
+    SMALL_ANNUAL_COST: '45',
+    SMALL_ANNUAL_URL: 'https://maptio.chargebee.com/hosted_pages/plans/annual_35_50',
+
+    STANDARD_MONTHLY_COST: '99',
+    STANDARD_MONTHLY_URL: 'https://maptio.chargebee.com/hosted_pages/plans/standard-plan',
+    STANDARD_ANNUAL_COST: '89',
+    STANDARD_ANNUAL_URL: 'https://maptio.chargebee.com/hosted_pages/plans/annual-standard-plan',
   }
 };
