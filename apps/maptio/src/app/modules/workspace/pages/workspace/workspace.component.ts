f26d15c2d2db878d312a269f88a4e496b355c16e
Romek Goj <roman.goj@gmail.com>
2022-05-16 11:13:24 +0100

    Reformat workspace TS code with prettier

--- apps/maptio/src/app/modules/workspace/pages/workspace/workspace.component.ts
+++ apps/maptio/src/app/modules/workspace/pages/workspace/workspace.component.ts
@@ -1,41 +1,39 @@
-
 import { tap } from 'rxjs/operators';
-import { BuildingComponent } from "../../components/data-entry/hierarchy/building.component";
-import { DataService, CounterService } from "../../services/data.service";
-import { RoleLibraryService } from "../../services/role-library.service";
-import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
-import { Subscription, Subject } from "rxjs";
-import { Initiative } from "../../../../shared/model/initiative.data";
-import { DataSet } from "../../../../shared/model/dataset.data";
-import { Team } from "../../../../shared/model/team.data";
-import { EmitterService } from "../../../../core/services/emitter.service";
-import { DatasetFactory } from "../../../../core/http/map/dataset.factory";
-import { TeamFactory } from "../../../../core/http/team/team.factory";
-import { ViewChild } from "@angular/core";
+import { BuildingComponent } from '../../components/data-entry/hierarchy/building.component';
+import { DataService, CounterService } from '../../services/data.service';
+import { RoleLibraryService } from '../../services/role-library.service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Subscription, Subject } from 'rxjs';
+import { Initiative } from '../../../../shared/model/initiative.data';
+import { DataSet } from '../../../../shared/model/dataset.data';
+import { Team } from '../../../../shared/model/team.data';
+import { EmitterService } from '../../../../core/services/emitter.service';
+import { DatasetFactory } from '../../../../core/http/map/dataset.factory';
+import { TeamFactory } from '../../../../core/http/team/team.factory';
+import { ViewChild } from '@angular/core';
 import {
-  Component, OnInit, OnDestroy,
+  Component,
+  OnInit,
+  OnDestroy,
   ChangeDetectorRef,
-  ChangeDetectionStrategy
-} from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
-import { User } from "../../../../shared/model/user.data";
-import { Tag } from "../../../../shared/model/tag.data";
-import { Role } from "../../../../shared/model/role.data";
-import { Intercom } from "ng-intercom";
-import { Angulartics2Mixpanel } from "angulartics2/mixpanel";
+  ChangeDetectionStrategy,
+} from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { User } from '../../../../shared/model/user.data';
+import { Tag } from '../../../../shared/model/tag.data';
+import { Role } from '../../../../shared/model/role.data';
+import { Intercom } from 'ng-intercom';
+import { Angulartics2Mixpanel } from 'angulartics2/mixpanel';
 
 @Component({
-  selector: "workspace",
-  templateUrl: "workspace.component.html",
-  styleUrls: ["./workspace.component.css"],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  selector: 'workspace',
+  templateUrl: 'workspace.component.html',
+  styleUrls: ['./workspace.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-
-
 export class WorkspaceComponent implements OnInit, OnDestroy {
-
-  @ViewChild("building", { static: true })
-  buildingComponent: BuildingComponent
+  @ViewChild('building', { static: true })
+  buildingComponent: BuildingComponent;
 
   public isBuildingPanelCollapsed: boolean = true;
   public isDetailsPanelCollapsed: boolean = true;
@@ -57,7 +55,7 @@ export class WorkspaceComponent implements OnInit, OnDestroy {
   public user: User;
   public roles: Role[];
   public canvasYMargin: number;
-  public canvasHeight: number
+  public canvasHeight: number;
 
   public openedNode: Initiative;
   public openedNodeParent: Initiative;
@@ -69,60 +67,76 @@ export class WorkspaceComponent implements OnInit, OnDestroy {
   teamId: string;
   selectableTags: Array<Tag>;
 
-  @ViewChild("dragConfirmation")
+  @ViewChild('dragConfirmation')
   dragConfirmationModal: NgbModal;
 
   ngOnDestroy(): void {
-    EmitterService.get("currentTeam").emit(undefined)
+    EmitterService.get('currentTeam').emit(undefined);
     if (this.routeSubscription) this.routeSubscription.unsubscribe();
     if (this.userSubscription) this.userSubscription.unsubscribe();
   }
 
-  constructor(private route: ActivatedRoute, private datasetFactory: DatasetFactory,
-    private teamFactory: TeamFactory, private dataService: DataService, private roleLibrary: RoleLibraryService,
-    private cd: ChangeDetectorRef, private mixpanel: Angulartics2Mixpanel, private intercom: Intercom) {
-
-  }
+  constructor(
+    private route: ActivatedRoute,
+    private datasetFactory: DatasetFactory,
+    private teamFactory: TeamFactory,
+    private dataService: DataService,
+    private roleLibrary: RoleLibraryService,
+    private cd: ChangeDetectorRef,
+    private mixpanel: Angulartics2Mixpanel,
+    private intercom: Intercom
+  ) {}
 
   ngOnInit() {
     this.isLoading = true;
     this.cd.markForCheck();
-    this.routeSubscription = this.route.data.pipe(
-      tap((data) => {
-        let newDatasetId = data.data.dataset.datasetId;
-        if (newDatasetId !== this.datasetId) {
-          this.isBuildingPanelCollapsed = true;
-          this.isDetailsPanelCollapsed = true;
-          // this.closeDetailsPanel();
-          // this.closeBuildingPanel();
-          this.cd.markForCheck()
-        }
-      }),
-      tap((data: { data: { dataset: DataSet, team: Team, members: User[], user: User } }) => {
-        this.isLoading = true;
-        this.cd.markForCheck();
-        return this.buildingComponent.loadData(data.data.dataset, data.data.team, data.data.members)
-          .then(() => {
-            this.isLoading = false;
+    this.routeSubscription = this.route.data
+      .pipe(
+        tap((data) => {
+          let newDatasetId = data.data.dataset.datasetId;
+          if (newDatasetId !== this.datasetId) {
+            this.isBuildingPanelCollapsed = true;
+            this.isDetailsPanelCollapsed = true;
+            // this.closeDetailsPanel();
+            // this.closeBuildingPanel();
             this.cd.markForCheck();
-          });
-      }))
-      .subscribe((data: { data: { dataset: DataSet, team: Team, members: User[], user: User } }) => {
-
-        this.dataset = data.data.dataset;
-        this.tags = data.data.dataset.tags;
-        this.team = data.data.team;
-        this.members = data.data.members;
-        this.user = data.data.user;
-        this.datasetId = this.dataset.datasetId;
-        this.teamName = this.team.name;
-        this.teamId = this.team.team_id;
-        EmitterService.get("currentTeam").emit(this.team);
-        this.isEmptyMap = !this.dataset.initiative.children || this.dataset.initiative.children.length === 0;
-        this.cd.markForCheck();
-      });
+          }
+        }),
+        tap(
+          (data: {
+            data: { dataset: DataSet; team: Team; members: User[]; user: User };
+          }) => {
+            this.isLoading = true;
+            this.cd.markForCheck();
+            return this.buildingComponent
+              .loadData(data.data.dataset, data.data.team, data.data.members)
+              .then(() => {
+                this.isLoading = false;
+                this.cd.markForCheck();
+              });
+          }
+        )
+      )
+      .subscribe(
+        (data: {
+          data: { dataset: DataSet; team: Team; members: User[]; user: User };
+        }) => {
+          this.dataset = data.data.dataset;
+          this.tags = data.data.dataset.tags;
+          this.team = data.data.team;
+          this.members = data.data.members;
+          this.user = data.data.user;
+          this.datasetId = this.dataset.datasetId;
+          this.teamName = this.team.name;
+          this.teamId = this.team.team_id;
+          EmitterService.get('currentTeam').emit(this.team);
+          this.isEmptyMap =
+            !this.dataset.initiative.children ||
+            this.dataset.initiative.children.length === 0;
+          this.cd.markForCheck();
+        }
+      );
   }
-
 
   saveDetailChanges() {
     this.buildingComponent.saveChanges();
@@ -136,17 +150,20 @@ export class WorkspaceComponent implements OnInit, OnDestroy {
   //     this.cd.markForCheck();
   // }
 
-  saveChanges(change: { initiative: Initiative, tags: Array<Tag> }) {
-    this.isEmptyMap = !change.initiative.children || change.initiative.children.length === 0;
+  saveChanges(change: { initiative: Initiative; tags: Array<Tag> }) {
+    this.isEmptyMap =
+      !change.initiative.children || change.initiative.children.length === 0;
     this.isSaving = true;
     this.cd.markForCheck();
 
     this.dataset.initiative = change.initiative;
     this.dataset.tags = change.tags;
-    this.tags = change.tags
+    this.tags = change.tags;
 
-    let depth = 0
-    change.initiative.traverse((n) => { depth++ });
+    let depth = 0;
+    change.initiative.traverse((n) => {
+      depth++;
+    });
 
     // Ensure that that the dataset and team versions of the role library are identical before saving
     const roleLibrary = this.roleLibrary.getRoles();
@@ -157,13 +174,35 @@ export class WorkspaceComponent implements OnInit, OnDestroy {
       this.datasetFactory.upsert(this.dataset, this.datasetId),
       this.teamFactory.upsert(this.team),
     ])
-      .then(([hasSavedDataset, hasSavedTeam]: [boolean, boolean]) => {
-        this.dataService.set({ initiative: change.initiative, dataset: this.dataset, team: this.team, members: this.members });
-        return hasSavedDataset && hasSavedTeam;
-      }, (reason) => { console.error(reason) })
+      .then(
+        ([hasSavedDataset, hasSavedTeam]: [boolean, boolean]) => {
+          this.dataService.set({
+            initiative: change.initiative,
+            dataset: this.dataset,
+            team: this.team,
+            members: this.members,
+          });
+          return hasSavedDataset && hasSavedTeam;
+        },
+        (reason) => {
+          console.error(reason);
+        }
+      )
       .then(() => {
-        this.intercom.trackEvent("Editing map", { team: this.team.name, teamId: this.team.team_id, datasetId: this.datasetId, mapName: change.initiative.name, circles: depth });
-        this.mixpanel.eventTrack("Editing map", { team: this.team.name, teamId: this.team.team_id, datasetId: this.datasetId, mapName: change.initiative.name, circles: depth });
+        this.intercom.trackEvent('Editing map', {
+          team: this.team.name,
+          teamId: this.team.team_id,
+          datasetId: this.datasetId,
+          mapName: change.initiative.name,
+          circles: depth,
+        });
+        this.mixpanel.eventTrack('Editing map', {
+          team: this.team.name,
+          teamId: this.team.team_id,
+          datasetId: this.datasetId,
+          mapName: change.initiative.name,
+          circles: depth,
+        });
         return;
       })
       .then(() => {
@@ -171,9 +210,6 @@ export class WorkspaceComponent implements OnInit, OnDestroy {
         this.cd.markForCheck();
       });
   }
-
-
-
 
   toggleEditMode() {
     this.isEditMode = !this.isEditMode;
@@ -186,7 +222,7 @@ export class WorkspaceComponent implements OnInit, OnDestroy {
     this.cd.markForCheck();
   }
 
-  addInitiative(data: { node: Initiative, subNode: Initiative }) {
+  addInitiative(data: { node: Initiative; subNode: Initiative }) {
     this.buildingComponent.addNodeTo(data.node, data.subNode);
   }
 
@@ -194,7 +230,15 @@ export class WorkspaceComponent implements OnInit, OnDestroy {
     this.buildingComponent.removeNode(node);
   }
 
-  moveInitiative({ node, from, to }: { node: Initiative, from: Initiative, to: Initiative }) {
+  moveInitiative({
+    node,
+    from,
+    to,
+  }: {
+    node: Initiative;
+    from: Initiative;
+    to: Initiative;
+  }) {
     this.buildingComponent.moveNode(node, from, to);
   }
 
@@ -202,7 +246,6 @@ export class WorkspaceComponent implements OnInit, OnDestroy {
     this.isDetailsPanelCollapsed = false;
     // this.resizeMap();
     this.cd.markForCheck();
-
   }
 
   closeDetailsPanel() {
@@ -230,7 +273,7 @@ export class WorkspaceComponent implements OnInit, OnDestroy {
 
   onEditTags() {
     this.isBuildingPanelCollapsed = false;
-    this.buildingComponent.tabs.select("tags-tab");
+    this.buildingComponent.tabs.select('tags-tab');
     this.cd.markForCheck();
   }
 
