16eec9088e4ea4af8f92aa33f55dffd543c12d4c
Romek Goj <roman.goj@gmail.com>
2022-05-17 16:10:43 +0100

    Show the newly scaffolded component in panel

--- apps/maptio/src/app/modules/onboarding-message/onboarding-message.module.ts
+++ apps/maptio/src/app/modules/onboarding-message/onboarding-message.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { OnboardingMessageComponent } from './onboarding-message/onboarding-message.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    OnboardingMessageComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    OnboardingMessageComponent
   ]
 })
 export class OnboardingMessageModule { }
