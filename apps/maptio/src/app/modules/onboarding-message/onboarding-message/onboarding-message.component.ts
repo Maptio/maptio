7d731c2a9608760865327dfd7a8bdb71163ec839
Romek Goj <roman.goj@gmail.com>
2022-05-18 16:58:35 +0100

    Show onboarding messages based on user progress

--- apps/maptio/src/app/modules/onboarding-message/onboarding-message/onboarding-message.component.ts
+++ apps/maptio/src/app/modules/onboarding-message/onboarding-message/onboarding-message.component.ts
@@ -1,7 +1,9 @@
 import { Component, Input } from '@angular/core';
 
+import { BehaviorSubject, combineLatest } from 'rxjs';
 import { map } from 'rxjs/operators';
 
+import { UserRole } from '@maptio-shared/model/permission.data';
 import { UserService } from '@maptio-shared/services/user/user.service';
 
 
@@ -13,8 +15,29 @@ import { UserService } from '@maptio-shared/services/user/user.service';
 export class OnboardingMessageComponent {
   @Input() floating = false;
 
-  showMessage$ = this.userService.user$.pipe(
-    map(user => user.onboardingProgress['showEditingPanelMessage'])
+  messageKey$ = new BehaviorSubject<string>('');
+  @Input() set messageKey(messageKey: string) {
+    this.messageKey$.next(messageKey);
+  }
+
+  // Show message only if user has not already dismissed it and only if they
+  // are an admin
+  showMessage$ = combineLatest([
+    this.messageKey$,
+    this.userService.user$,
+  ]).pipe(
+    map(([messageKey, user]) => {
+      if (
+        user &&
+        messageKey &&
+        user.userRole === UserRole.Admin &&
+        Object.prototype.hasOwnProperty.call(user.onboardingProgress, messageKey)
+      ) {
+        return user.onboardingProgress[messageKey];
+      } else {
+        return false;
+      }
+    })
   );
 
   constructor(public userService: UserService) {};
