ef3f60f29b3749ecabc3e242d9b4c049a3a7f4db
Romek Goj <roman.goj@gmail.com>
2022-05-23 16:03:43 +0100

    Fix mixing and matching old and new consts files

--- apps/maptio/src/app/core/footer/footer.component.ts
+++ apps/maptio/src/app/core/footer/footer.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input} from "@angular/core";
 
 import { environment } from "@maptio-environment";
+import { environment as config } from "@maptio-config/environment";
 
 @Component({
     selector: "footer",
@@ -13,8 +14,8 @@ export class FooterComponent {
 
     public TERMS_AND_CONDITIONS_URL: string = environment.TERMS_AND_CONDITIONS_URL
     public PRIVACY_POLICY_URL: string = environment.PRIVACY_POLICY_URL
-    public BLOG_URL: string = environment.BLOG_URL;
-    public WIKI_URL: string = environment.WIKI_URL;
+    public BLOG_URL: string = config.BLOG_URL;
+    public WIKI_URL: string = config.WIKI_URL;
     public today  = Date.now();
 
     constructor() { }
