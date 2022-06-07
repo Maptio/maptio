3f0be9dd753128525f777acbab0dfddae8e8b190
Romek Goj <roman.goj@gmail.com>
2022-06-07 12:00:09 +0100

    Run OpenReplay outside Angular from a service

--- apps/maptio/src/app/shared/services/open-replay.service.ts
+++ apps/maptio/src/app/shared/services/open-replay.service.ts
@@ -1,13 +1,34 @@
-import { Injectable } from '@angular/core';
+import { Injectable, NgZone } from '@angular/core';
+
+import Tracker from '@openreplay/tracker';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class OpenReplayService {
+  public tracker?: Tracker | null;
 
-  constructor() { }
+  constructor(private zone: NgZone) {
+    this.zone.runOutsideAngular(() => {
+      this.tracker = new Tracker({
+        projectKey: "XchYPkjBhtyztTjamVfo",
+
+        // Privacy settings
+        respectDoNotTrack: true,
+        obscureTextEmails: true,
+        obscureTextNumbers: true,
+        obscureInputEmails: true,
+        defaultInputMode: 1,
+      });
+    });
+  }
 
   public start() {
-    console.log('Starting OpenReplay service');
+    this.zone.runOutsideAngular(() => {
+      if (this.tracker) {
+        return this.tracker.start();
+      }
+    });
   }
 }
