f1fde5d8f12ead49c15d9e2f6e1b2436b6e116f5
Romek Goj <roman.goj@gmail.com>
2022-08-08 08:37:50 +0100

    Return to the same page when language is changed

--- apps/maptio/src/app/core/header/language-picker.component.ts
+++ apps/maptio/src/app/core/header/language-picker.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'maptio-language-picker',
   templateUrl: './language-picker.component.html',
-  styleUrls: ['./language-picker.component.scss']
+  styleUrls: ['./language-picker.component.scss'],
 })
-export class LanguagePickerComponent implements OnInit {
+export class LanguagePickerComponent {
+  currentLocation: string;
 
-  constructor() { }
+  constructor(private location: Location) {}
 
-  ngOnInit(): void {
+  onClick() {
+    this.currentLocation = this.location.path();
   }
-
 }
