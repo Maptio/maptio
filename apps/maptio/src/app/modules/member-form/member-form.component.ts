4f4f9e3db57cf7782d5639bbcc1e96d8b9c9bec6
Romek Goj <roman.goj@gmail.com>
2022-08-23 11:09:25 +0100

    Add more error translations

--- apps/maptio/src/app/modules/member-form/member-form.component.ts
+++ apps/maptio/src/app/modules/member-form/member-form.component.ts
@@ -228,7 +228,9 @@ export class MemberFormComponent implements OnInit {
           return this.createdUser;
         },
         (reason) => {
-          return Promise.reject(`Can't create ${this.email} : ${reason}`);
+          return Promise.reject(
+            $localize`Can't create ${this.email} : ${reason}`
+          );
         }
       )
       .then((user: User) => {
@@ -336,7 +338,7 @@ export class MemberFormComponent implements OnInit {
 
   async onMergeDuplicateUsers() {
     if (!(this.member instanceof User)) {
-      this.errorMessage = `
+      this.errorMessage = $localize`
         The application encountered an unexpected input error while attempting
         user de-duplication. Please contact us for help and quote this error
         message.
@@ -360,7 +362,7 @@ export class MemberFormComponent implements OnInit {
         'Error while attempting to replace user with duplicate',
         error
       );
-      this.errorMessage = `
+      this.errorMessage = $localize`
         The application encountered an unexpected error while attempting user
         de-duplication. Please contact us for help and quote this error
         message.
