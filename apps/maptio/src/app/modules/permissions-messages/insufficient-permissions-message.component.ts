244892cfd44b0b45a2e0cc9a99ba85588988901d
Romek Goj <roman.goj@gmail.com>
2022-09-01 12:57:37 +0100

    Move and improve insufficient permissions message

--- apps/maptio/src/app/modules/permissions-messages/insufficient-permissions-message.component.ts
+++ apps/maptio/src/app/modules/permissions-messages/insufficient-permissions-message.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { environment } from '@maptio-config/environment';
 
 @Component({
   selector: 'maptio-insufficient-permissions-message',
   templateUrl: './insufficient-permissions-message.component.html',
-  styleUrls: ['./insufficient-permissions-message.component.scss']
+  styleUrls: ['./insufficient-permissions-message.component.scss'],
 })
-export class InsufficientPermissionsMessageComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class InsufficientPermissionsMessageComponent {
+  KB_URL_PERMISSIONS = environment.KB_URL_PERMISSIONS;
 }
