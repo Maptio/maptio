b82a5ca246cd495d0fe265ddf5999e658e2ae2df
Romek Goj <roman.goj@gmail.com>
2022-09-01 12:20:23 +0100

    Scaffold component for no permissions message

---
+++ apps/maptio/src/app/modules/permissions/insufficient-permissions-message.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'maptio-insufficient-permissions-message',
+  templateUrl: './insufficient-permissions-message.component.html',
+  styleUrls: ['./insufficient-permissions-message.component.scss']
+})
+export class InsufficientPermissionsMessageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ apps/maptio/src/app/modules/permissions/permissions.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { InsufficientPermissionsMessageComponent } from './insufficient-permissions-message.component';
+
+@NgModule({
+  declarations: [InsufficientPermissionsMessageComponent],
+  imports: [CommonModule],
+})
+export class PermissionsModule {}
