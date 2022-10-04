70c8d81ec326e6a5ad797146fb4abb5a66637f20
Romek Goj <roman.goj@gmail.com>
2022-10-04 18:04:20 +0100

    Fix function getting locale from path

--- apps/maptio-server/src/i18n/get-locale-from-path.ts
+++ apps/maptio-server/src/i18n/get-locale-from-path.ts
@@ -3,5 +3,5 @@ export function getLocaleFromPath(path: string, supportedLocales: string[]) {
   const suspectedLocale = pathComponents[1];
   const isLocaleInPath = supportedLocales.includes(suspectedLocale);
 
-  return isLocaleInPath ? suspectedLocale : null;
+  return isLocaleInPath ? suspectedLocale : '';
 }
