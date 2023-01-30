3ae9371aaebd1f5f464caf20ebd022d30d3e2641
Romek Goj <roman.goj@gmail.com>
2023-01-30 09:57:29 +0000

    Make the permissions guard reactive

--- apps/maptio/src/app/core/guards/permission.guard.ts
+++ apps/maptio/src/app/core/guards/permission.guard.ts
@@ -1,4 +1,6 @@
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
 import { Permissions } from '../../shared/model/permission.data';
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot } from '@angular/router';
@@ -20,15 +22,17 @@ export class PermissionGuard implements CanActivate, CanActivateChild {
     const currentOrganisationId: string = route.parent.paramMap.get('teamid');
     this.store.dispatch(setCurrentOrganisationId({ currentOrganisationId }));
 
-    const userPermissions = this.permissionsService.getUserPermissions();
+    return this.permissionsService.userPermissions$.pipe(
+      map((userPermissions) => {
+        route.data.permissions.forEach((required: Permissions) => {
+          if (!userPermissions.includes(required)) {
+            this.router.navigate(['/unauthorized']);
+          }
+        });
 
-    route.data.permissions.forEach((required: Permissions) => {
-      if (!userPermissions.includes(required)) {
-        this.router.navigate(['/unauthorized']);
-      }
-    });
-
-    return of(true);
+        return true;
+      })
+    );
   }
 
   canActivateChild(childRoute: ActivatedRouteSnapshot): Observable<boolean> {
