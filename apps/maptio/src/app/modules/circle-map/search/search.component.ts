130d20e2f843f95f683023dfe0bfe7248f72efc4
Romek Goj <roman.goj@gmail.com>
2023-02-27 15:55:50 +0000

    Flatten list of initiatives

--- apps/maptio/src/app/modules/circle-map/search/search.component.ts
+++ apps/maptio/src/app/modules/circle-map/search/search.component.ts
@@ -1,8 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 
 import { Observable } from 'rxjs';
 import { map, startWith } from 'rxjs/operators';
+
+import {
+  InitiativeNode,
+  InitiativeViewModel,
+} from '@maptio-circle-map/initiative.model';
 
 @Component({
   selector: 'maptio-search',
@@ -10,21 +15,42 @@ import { map, startWith } from 'rxjs/operators';
   styleUrls: ['./search.component.scss'],
 })
 export class SearchComponent implements OnInit {
+  @Input() rootCircle: InitiativeNode;
+
+  initiatives: InitiativeViewModel[];
+
   myControl = new FormControl('');
   options: string[] = ['One', 'Two', 'Three'];
   filteredOptions: Observable<string[]>;
 
   ngOnInit() {
+    this.initiatives = this.flattenInitiatives(this.rootCircle);
+
     this.filteredOptions = this.myControl.valueChanges.pipe(
       startWith(''),
-      map((value) => this._filter(value || ''))
+      map((value) => this.filterInitiatives(value || ''))
     );
   }
 
-  private _filter(value: string): string[] {
+  private flattenInitiatives(
+    rootCircle: InitiativeNode
+  ): InitiativeViewModel[] {
+    // Not starting with the root circle itself as it's not really a circle on
+    // the map
+    const initiatives = [];
+
+    if (rootCircle.children) {
+      rootCircle.children.forEach((child) => {
+        initiatives.push(...this.flattenInitiatives(child));
+      });
+    }
+    return initiatives;
+  }
+
+  private filterInitiatives(value: string): string[] {
     if (value === '') {
       // Simulate typeahead behavior
-      return [];
+      return this.initiatives.map((initiative) => initiative.name).slice(0, 5);
     }
 
     const filterValue = value.toLowerCase();
