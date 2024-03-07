b799c8f38c89b7d191f3ac8eaf3a426573fb6c93
Romek Goj <roman.goj@gmail.com>
2024-03-07 16:19:03 +0000

    Scaffold a new component to build new panels in

---
+++ apps/maptio/src/app/preview/preview.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'maptio-preview',
+  standalone: true,
+  templateUrl: './preview.component.html',
+  styleUrls: ['./preview.component.scss'],
+})
+export class PreviewComponent {}
